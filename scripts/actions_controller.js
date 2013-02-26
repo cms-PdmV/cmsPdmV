4a5e1307021bda98fdd68118d18e659da3ad68aa
anorkus <anorkus@4525493e-7705-40b1-a816-d608a930855b>
2013-02-26 16:47:31 +0000

    Updated Actions page. With not tested commit updated action possibility
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSUserCode/trunk/prep2/nikolas/prep2@524 4525493e-7705-40b1-a816-d608a930855b

--- scripts/actions_controller.js
+++ scripts/actions_controller.js
@@ -4,15 +4,16 @@ function resultsCtrl($scope, $http, $location){
         {text:'Actions',select:true, db_name:'prepid'}
 //         {text:'Actions',select:true, db_name:''},
     ];
-//     $scope.chained_campaigns = ["prepid"];
     $scope.campaigns = ["------"];
     $scope.selectedOption = "------";
-     $scope.selected_campaign = "";
+    $scope.selected_campaign = "";
+//     $scope.chained_requests = [];
+//     $scope.chained_campaigns = [];
+//     $scope.toBeUpdated = {};
 //     $scope.watch("selected_campaign", function(){
 //        console.log("selected campaign pasikeite:", $scope.selected_campaign); 
 //     });
     $scope.select_campaign = function(){
-        console.log("pasikeite campaign:", $scope.selectedOption);
         console.log($scope.actions_defaults);
         console.log($scope.chained_campaigns);
         //set the well to have only ChainedCampaigns which includes selectedOption
@@ -89,13 +90,10 @@ function resultsCtrl($scope, $http, $location){
     }
     var promise = $http.get('search/?db_name=chained_campaigns&query=""&page=-1')
     promise.then(function(data){
-        $scope.chained_campaigns = data.data.results;
+//         $scope.chained_campaigns = data.data.results;
         _.each(data.data.results, function(v){
-//             $scope.chained_campaigns.push(v._id);
             $scope.actions_defaults.push({text:v._id, select:true, db_name:v._id});
-//             console.log('chained',v._id);
         });
-//         console.log($scope.actions_defaults);
     });
 
     promise = $http.get('restapi/campaigns/get_all')
@@ -103,9 +101,13 @@ function resultsCtrl($scope, $http, $location){
         _.each(data.data.results, function(v){
            $scope.campaigns.push(v.value);
         });
-//         console.log($scope.campaigns);
     });
-
+//     promise = $http.get('search/?db_name=chained_requests&query=""&page=-1')
+//     promise.then(function(data){
+//        $scope.chained_requests = data.data.results;
+//     });
+// //     console.log("CR: ",$scope.chained_requests);
+// //     console.log("CC : ",$scope.chained_campaigns);
   $scope.showing_well = function(){
         if ($scope.show_well){
           $scope.show_well = false;
@@ -173,48 +175,94 @@ function resultsCtrl($scope, $http, $location){
        sort.descending = false;
      }
   };
+  $scope.showingAction = function(member_of_campaign){
+    if ($scope.selectedOption == "------"){
+        return true;
+    }else{
+        if (member_of_campaign == $scope.selectedOption){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+  };
+  $scope.updatedValues = function(member_of_campaign){
+    console.log(member_of_campaign);
+//      console.log("CR: ",$scope.chained_requests);
+//      console.log("CC : ",$scope.chained_campaigns);
+  };
 }
 var testApp = angular.module('testApp',[]).config(function($locationProvider){$locationProvider.html5Mode(true);});
-testApp.directive("customPrepId", function ($rootScope) {
+testApp.directive("customPrepId", function ($rootScope, $http) {
     return {
         restrict: 'E',
         replace: true,
         require: "ngModel",
         link: function (scope, element, attr, ctrl) {
           ctrl.$render = function(){
+//             scope.chainCampaignValues = scope.chained_campaigns.member_of_campaign;
+//             scope.chainReqValues = scope.chained_campaigns.member_of_campaign;
             scope.actionInfo = ctrl.$viewValue;
-            console.log(scope.actionInfo);
-            scope.displayBox = true;
-          };
-          scope.showInput = function(){
+            scope.toBeUpdated = {block_number: "", staged:"",threshold:""}; //define a variable to localy bind data
             scope.displayBox = false;
           };
-          scope.closeInput = function(){
-            scope.displayBox = true;
-            console.log(scope.displayBox);
+          scope.showInput = function(){
+            if (scope.displayBox){
+              scope.displayBox = false;
+            } else{
+              scope.displayBox = true;
+            };
+          };
+          
+          scope.commit = function(){
+//             ctrl.$viewValue.block_number = scope.toBeUpdated.block_number;
+//             ctrl.$viewValue.staged = scope.toBeUpdated.staged;
+//             ctrl.$viewValue.threshold = scope.toBeUpdated.threshold;
+            scope.showInput();
+//             restapi/actions/update
+            $http({method:'GET', url:'/restapi/actions/update/',data:angular.toJson(scope.result)}).success(function(data,status){
+              console.log(data,status);
+//               $scope.update["success"] = data["results"];
+//               $scope.update["fail"] = false;
+//               $scope.update["status_code"] = status;
+            }).error(function(data,status){
+//               $scope.update["success"] = false;
+//               $scope.update["fail"] = true;
+//               $scope.update["status_code"] = status;
+                    alert("Error: ", status);
+            });
+            
           };
         },
         template:
-        '<div ng-switch="displayBox">'+
-        '  <div ng-switch-when="true">'+
-        '    <input type="checkbox" ng-disabled="actionInfo.member_of_campaign != selectedOption" ng-click="showInput();"/>'+
-        '  </div>'+
+        '<div ng-switch="actionInfo === "undefined"">'+
         '  <div ng-switch-when="false">'+
-        '    <input type="button" class="btn" ng-click="closeInput();" value="-"/>'+
-        '    <select class="input-mini" style="margin-bottom: 0px; margin-left: 2px;">'+
-        '      <option ng-repeat="key in [0,1,2,3,4,5,6]" ng-selected ="value[0] == key">{{key}}</option>'+
-        '    </select>'+
-        '    <input type="number" style="margin-bottom: 0px; width: 80px;"/>'+
-        '    <span class="input-append">'+
-        '      <input type="number" style="margin-bottom: 0px; width: 25px;"/>'+
-        '      <span class="add-on">%</span>'+
-        '    </span>'+
+//         '    {{toBeUpdated | json}}'+
+        '      <input type="checkbox" ng-model="actionInfo.flag"/>'+
+        '      <a ng-click="showInput();" ng-hide="displayBox">'+
+        '        <i class="icon-wrench"></i>'+
+        '      </a>'+
+         '    <div ng-show="displayBox">'+
+        '      <select class="input-mini" style="margin-bottom: 0px; margin-left: 2px;" ng-model="actionInfo.block_number">'+
+        '        <option ng-repeat="key in [0,1,2,3,4,5,6]">{{key}}</option>'+
+        '      </select>'+
+        '      <input type="number" style="margin-bottom: 0px; width: 80px;" ng-model="actionInfo.threshold"/>'+
+        '      <span class="input-append">'+
+        '        <input type="number" style="margin-bottom: 0px; width: 25px;" ng-model="actionInfo.staged" ngMaxlength=3/>'+
+        '        <span class="add-on">%</span>'+
+        '      </span>'+
+        '      <a ng-click="commit();">'+
+        '        <i class="icon-envelope"></i>'+
+        '      </a>'+
+        '    </div>'+
+        '    <text>'+
+//         '      {{actionInfo}}'+
+//         '      {{selectedOption}}'+
+        '    </text>'+
         '  </div>'+
-        '  <text>'+
-        '{{actionInfo.member_of_campaign == selectedOption}}'+
-        '  {{actionInfo.member_of_campaign}}'+
-        '  {{selectedOption}}'+
-        '  </text>'+
+        '  <div ng-switch-when="true">'+
+        '  </div>'+
         '</div>'
     };
 });
