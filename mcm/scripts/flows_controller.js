e7baed7cd4c3fb3b4fe69d720d52cfbf99075ee1
PdmV Service <pdmv.service@cern.ch>
2013-04-26 14:59:52 +0200

    N found. remove query

--- mcm/scripts/flows_controller.js
+++ mcm/scripts/flows_controller.js
@@ -1,184 +1,180 @@
 function resultsCtrl($scope, $http, $location, $window){
-    $scope.flows_defaults = [
-        {text:'PrepId',select:true, db_name:'prepid'},
-        {text:'Actions',select:true, db_name:''},
-        {text:'Approval',select:true, db_name:'approval'},
-        {text:'Allowed Campaigns',select:true, db_name:'allowed_campaigns'},
-        {text:'Next Campaign',select:true, db_name:'next_campaign'},
-    ];
-    $scope.user = {name: "guest", role:"user"}
-// GET username and role
-      var promise = $http.get("restapi/users/get_roles");
-       promise.then(function(data){
-        $scope.user.name = data.data.username;
-        $scope.user.role = data.data.roles[0];
-        $scope.user.roleIndex = parseInt(data.data.role_index);
-    }, function(data){
-        alert("Error getting user information. Error: "+data.status);
-    });
-// Endo of user info request
-    $scope.update = [];
-    $scope.show_well = false;
-    $scope.chained_campaigns = [];
-    if ($location.search()["db_name"] === undefined){
-      $scope.dbName = "flows";
-    }else{
-      $scope.dbName = $location.search()["db_name"];
-    }
-    if($location.search()["query"] === undefined){
-      $location.search("query",'""');
-    }
-    $scope.underscore = _;
-    $scope.selectedAll = false;
+  $scope.flows_defaults = [
+    {text:'PrepId',select:true, db_name:'prepid'},
+    {text:'Actions',select:true, db_name:''},
+    {text:'Approval',select:true, db_name:'approval'},
+    {text:'Allowed Campaigns',select:true, db_name:'allowed_campaigns'},
+    {text:'Next Campaign',select:true, db_name:'next_campaign'},
+  ];
+  $scope.update = [];
+  $scope.show_well = false;
+  $scope.chained_campaigns = [];
+  if ($location.search()["db_name"] === undefined){
+    $scope.dbName = "flows";
+  }else{
+    $scope.dbName = $location.search()["db_name"];
+  }
 
-    if($location.search()["page"] === undefined){
-        page = 0;
-        $location.search("page", 0);
-        $scope.list_page = 0;
-    }else{
-        page = $location.search()["page"];
-        $scope.list_page = parseInt(page);
-    }
-    $scope.approvalIcon = function(value){
-	icons = { 'none':'icon-off',
+  $scope.underscore = _;
+  $scope.selectedAll = false;
+
+  if($location.search()["page"] === undefined){
+    page = 0;
+    $location.search("page", 0);
+    $scope.list_page = 0;
+  }else{
+    page = $location.search()["page"];
+    $scope.list_page = parseInt(page);
+  }
+
+  $scope.approvalIcon = function(value){
+    icons = { 'none':'icon-off',
 		  'flow' : 'icon-share',
 		  'submit' : 'icon-ok'
-	}
-	if (icons[value]){
+	  }
+	  if (icons[value]){
 	    return icons[value];
-	}else{
+	  }else{
 	    return  "icon-question-sign";
-	}
-    };    
+	  }
+  };    
     
-    $scope.delete_object = function(db, value){
-        $http({method:'DELETE', url:'restapi/'+db+'/delete/'+value}).success(function(data,status){
-            if (data["results"]){
-                $scope.update["success"] = data.results;
-                $scope.update["fail"] = false;
-                $scope.update["status_code"] = status;
-                $window.location.reload();
-            }else{
-                $scope.update["success"] = false;
-                $scope.update["fail"] = true;
-                $scope.update["status_code"] = status;
-            }
-        }).error(function(status){
-            alert('Error no.' + status + '. Could not delete object.');
-        });
-    };
-    $scope.next_step = function(prepid){
-      $http({method:'GET', url:'restapi/'+$scope.dbName+'/approve/'+prepid}).success(function(data,status){
+  $scope.delete_object = function(db, value){
+    $http({method:'DELETE', url:'restapi/'+db+'/delete/'+value}).success(function(data,status){
+      if (data["results"]){
         $scope.update["success"] = data.results;
         $scope.update["fail"] = false;
         $scope.update["status_code"] = status;
         $window.location.reload();
-      }).error(function(status){
+      }else{
         $scope.update["success"] = false;
         $scope.update["fail"] = true;
         $scope.update["status_code"] = status;
-      });
-    };
+      }
+    }).error(function(status){
+      alert('Error no.' + status + '. Could not delete object.');
+    });
+  };
 
-    $scope.reset_flow = function(prepid){
-      $http({method:'GET', url:'restapi/'+$scope.dbName+'/approve/'+prepid+'/0'}).success(function(data,status){
-        $scope.update["success"] = data.results;
-        $scope.update["fail"] = false;
-        $scope.update["status_code"] = status;
-        $window.location.reload();
-      }).error(function(status){
-        $scope.update["success"] = false;
-        $scope.update["fail"] = true;
-        $scope.update["status_code"] = status;
-      });
-    };
+  $scope.next_step = function(prepid){
+    $http({method:'GET', url:'restapi/'+$scope.dbName+'/approve/'+prepid}).success(function(data,status){
+      $scope.update["success"] = data.results;
+      $scope.update["fail"] = false;
+      $scope.update["status_code"] = status;
+      $window.location.reload();
+    }).error(function(status){
+      $scope.update["success"] = false;
+      $scope.update["fail"] = true;
+      $scope.update["status_code"] = status;
+    });
+  };
 
-    $scope.select_all_well = function(){
-      $scope.selectedCount = true;
-      var selectedCount = 0
-      _.each($scope.flows_defaults, function(elem){
-        if (elem.select){
-          selectedCount +=1;
-        }
-        elem.select = true;
-      });
-      if (selectedCount == _.size($scope.flows_defaults)){
+  $scope.reset_flow = function(prepid){
+    $http({method:'GET', url:'restapi/'+$scope.dbName+'/approve/'+prepid+'/0'}).success(function(data,status){
+      $scope.update["success"] = data.results;
+      $scope.update["fail"] = false;
+      $scope.update["status_code"] = status;
+      $window.location.reload();
+    }).error(function(status){
+      $scope.update["success"] = false;
+      $scope.update["fail"] = true;
+      $scope.update["status_code"] = status;
+    });
+  };
+
+  $scope.select_all_well = function(){
+    $scope.selectedCount = true;
+    var selectedCount = 0
+    _.each($scope.flows_defaults, function(elem){
+      if (elem.select){
+        selectedCount +=1;
+      }
+      elem.select = true;
+    });
+    if (selectedCount == _.size($scope.flows_defaults)){
       _.each($scope.flows_defaults, function(elem){
         elem.select = false;
       });
       $scope.flows_defaults[0].select = true; //set prepid to be enabled by default
       $scope.flows_defaults[1].select = true; // set actions to be enabled
+      $scope.flows_defaults[2].select = true; // set actions to be enabled
+      $scope.flows_defaults[3].select = true; // set actions to be enabled
+      $scope.flows_defaults[4].select = true; // set actions to be enabled
       $scope.selectedCount = false;
-      }
-    };
+    }
+  };
 
-    $scope.sort = {
-        column: 'prepid',
-        descending: false
-    };
+  $scope.sort = {
+    column: 'prepid',
+    descending: false
+  };
 
-    $scope.selectedCls = function(column) {
-        return column == $scope.sort.column && 'sort-' + $scope.sort.descending;
-    };
+  $scope.selectedCls = function(column) {
+    return column == $scope.sort.column && 'sort-' + $scope.sort.descending;
+  };
     
-    $scope.changeSorting = function(column) {
-        var sort = $scope.sort;
-        if (sort.column == column) {
-            sort.descending = !sort.descending;
-        } else {
-            sort.column = column;
-            sort.descending = false;
-        }
-    };
-    $scope.showing_well = function(){
-        if ($scope.show_well){
-          $scope.show_well = false;
-        }
-        else{
-            $scope.show_well = true;
-        }
-    };    
+  $scope.changeSorting = function(column) {
+    var sort = $scope.sort;
+    if (sort.column == column) {
+      sort.descending = !sort.descending;
+    }else{
+      sort.column = column;
+      sort.descending = false;
+    }
+  };
 
-   $scope.$watch('list_page', function(){
-      var promise = $http.get("search/?"+ "db_name="+$scope.dbName+"&query="+$location.search()["query"]+"&page="+page)
-      promise.then(function(data){
-        $scope.result = data.data.results; 
-        if ($scope.result.length != 0){
-          columns = _.keys($scope.result[0]);
-          rejected = _.reject(columns, function(v){return v[0] == "_";}); //check if charat[0] is _ which is couchDB value to not be shown
-          $scope.columns = _.sortBy(rejected, function(v){return v;});  //sort array by ascending order
-          _.each(rejected, function(v){
-            add = true;
-            _.each($scope.flows_defaults, function(column){
-              if (column.db_name == v){
-                add = false;
-              }
-            });
-            if (add){
-              $scope.flows_defaults.push({text:v[0].toUpperCase()+v.substring(1).replace(/\_/g,' '), select:false, db_name:v});
+  $scope.showing_well = function(){
+    if ($scope.show_well){
+      $scope.show_well = false;
+    }else{
+      $scope.show_well = true;
+    }
+  };
+
+  $scope.$watch('list_page', function(){
+    var query = ""
+      _.each($location.search(), function(value,key){
+        query += "&"+key+"="+value
+    });
+    $scope.got_results = false; //to display/hide the 'found n results' while reloading
+    var promise = $http.get("search/?"+ "db_name="+$scope.dbName+query);
+    promise.then(function(data){
+      $scope.got_results = true;
+      $scope.result = data.data.results;
+      if ($scope.result === undefined ){
+        alert('The following url-search key(s) is/are not valid : '+_.keys(data.data));
+        return; //stop doing anything if results are undefined
+      }
+      if ($scope.result.length != 0){
+        columns = _.keys($scope.result[0]);
+        rejected = _.reject(columns, function(v){return v[0] == "_";}); //check if charat[0] is _ which is couchDB value to not be shown
+        $scope.columns = _.sortBy(rejected, function(v){return v;});  //sort array by ascending order
+        _.each(rejected, function(v){
+          add = true;
+          _.each($scope.flows_defaults, function(column){
+            if (column.db_name == v){
+              add = false;
             }
           });
-        }
-      }, function(){ alert("Error"); });
+          if (add){
+            $scope.flows_defaults.push({text:v[0].toUpperCase()+v.substring(1).replace(/\_/g,' '), select:false, db_name:v});
+          }
+        });
+      }
+    }, function(){ alert("Error getting information"); });
   });
     
   $scope.previous_page = function(current_page){
-      if (current_page >-1){
-        $location.search("page", current_page-1);
-        $scope.list_page = current_page-1;
-      }
+    if (current_page >-1){
+      $location.search("page", current_page-1);
+      $scope.list_page = current_page-1;
+    }
   };
+
   $scope.next_page = function(current_page){
-      if ($scope.result.length !=0){
-        $location.search("page", current_page+1);
-        $scope.list_page = current_page+1;
-      }
-  };
-  $scope.role = function(priority){
-    if(priority > $scope.user.roleIndex){ //if user.priority < button priority then hide=true
-      return true;
-    }else{
-      return false;
+    if ($scope.result.length !=0){
+      $location.search("page", current_page+1);
+      $scope.list_page = current_page+1;
     }
   };
 }
