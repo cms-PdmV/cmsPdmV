b15ef217ab4102e43b7b72d6fae56d361b8c62c0
PdmV Service <pdmv.service@cern.ch>
2013-05-30 11:23:54 +0200

    shown button

--- mcm/scripts/chainRequests_controller.js
+++ mcm/scripts/chainRequests_controller.js
@@ -178,7 +178,9 @@ function resultsCtrl($scope, $http, $location, $window){
   $scope.$watch('list_page', function(){
     var query = ""
       _.each($location.search(), function(value,key){
-        query += "&"+key+"="+value
+      if (key!= 'shown'){
+        query += "&"+key+"="+value;
+      }
     });
     $scope.got_results = false; //to display/hide the 'found n results' while reloading
     var promise = $http.get("search/?"+ "db_name="+$scope.dbName+query);
@@ -200,11 +202,39 @@ function resultsCtrl($scope, $http, $location, $window){
             $scope.defaults.push({text:v[0].toUpperCase()+v.substring(1).replace(/\_/g,' '), select:false, db_name:v});
           }
         });
+        if ($location.search()["shown"] !== undefined){
+          binary_shown = parseInt($location.search()["shown"]).toString(2).split('').reverse().join(''); //make a binary string interpretation of shown number
+          _.each($scope.defaults, function(column){
+            column_index = $scope.defaults.indexOf(column);
+            binary_bit = binary_shown.charAt(column_index);
+            if (binary_bit!= ""){ //if not empty -> we have more columns than binary number length
+              if (binary_bit == 1){
+                column.select = true;
+              }else{
+                column.select = false;
+              }
+            }else{ //if the binary index isnt available -> this means that column "by default" was not selected
+              column.select = false;
+            }
+          });
+        }
       }
     }, function(){
        alert("Error getting information");
     });
   });
+
+  $scope.calculate_shown = function(){ //on chage of column selection -> recalculate the shown number
+    var bin_string = ""; //reconstruct from begining
+    _.each($scope.defaults, function(column){ //iterate all columns
+      if(column.select){
+        bin_string ="1"+bin_string; //if selected add 1 to binary interpretation
+      }else{
+        bin_string ="0"+bin_string;
+      }
+    });
+    $location.search("shown",parseInt(bin_string,2)); //put into url the interger of binary interpretation
+  };
   
   $scope.previous_page = function(current_page){
     if (current_page >-1){
@@ -303,7 +333,7 @@ function resultsCtrl($scope, $http, $location, $window){
 };
 
 // NEW for directive
-var testApp = angular.module('testApp', []).config(function($locationProvider){$locationProvider.html5Mode(true);});
+// var testApp = angular.module('testApp', []).config(function($locationProvider){$locationProvider.html5Mode(true);});
 testApp.directive("inlineEditable", function(){
   return{
       require: 'ngModel',
