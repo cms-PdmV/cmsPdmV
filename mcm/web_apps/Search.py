a75f7bd269bc89c64a48db110f6745b548788cca
PdmV Service <pdmvserv@preptest.cern.ch>
2013-05-30 11:02:57 +0200

    fix duplicate documents in search

--- mcm/web_apps/Search.py
+++ mcm/web_apps/Search.py
@@ -2,9 +2,10 @@
 
 import cherrypy
 import simplejson
-from Page import Page
-from couchdb_layer.prep_database import database
 
+#from Page import Page
+from couchdb_layer.prep_database import database
+import copy
 
 class Search(object):
 
@@ -16,17 +17,32 @@ class Search(object):
 		self.page = int(page)
 		return self.run_query() 
 
+		
 	def run_query(self):
+		def unique_res(query_result):
+			docids = map(lambda doc : doc['_id'] , query_result)
+			docids_s = list(set(docids))
+			if len(docids) != len(docids_s):
+				docids_s = []
+				return_dict= copy.deepcopy( query_result )
+				for doc in query_result:
+					if not doc['_id'] in docids_s:
+						docids_s.append(doc['_id'])
+					else:
+						return_dict.remove(doc)		
+				return return_dict
+			return query_result
+
 		if len(self.query_list):
 			results_list=[]
 			##make each query separately and retrieve only the doc with counting == len(query_list)
 			for (i,query) in enumerate(self.query_list):
 				res = self.db.query(query, page_num=-1)
-				query_result = map(lambda r : r['value'], res)
+				query_result = unique_res( map(lambda r : r['value'], res) )
 				if i!=0:
 					## get only the one already in the intersection
-					id_list = map(lambda doc : doc['prepid'], results_list)
-					results_list = filter(lambda doc : doc['prepid'] in id_list, query_result)
+					id_list = map(lambda doc : doc['_id'], results_list)
+					results_list = filter(lambda doc : doc['_id'] in id_list, query_result)
 				else:
 					results_list= query_result
 			results = { 'results' : results_list}
@@ -40,8 +56,8 @@ class Search(object):
 				res = self.db.get_all(page_num=self.page)
 			else:
 				res = self.db.query(self.query, page_num=self.page)
-			for r in res:
-				results['results'].append(r['value']) 
+			query_result = unique_res( map(lambda r : r['value'], res) )
+			results['results'] = query_result
 			final = simplejson.dumps(results)
 			return final
 
@@ -72,6 +88,7 @@ class Search(object):
 
 		if len(args):
 			return simplejson.dumps(args)
+			#return simplejson.dumps(design['views'].keys())
 		return self.search(db_name, query, page, query_list)
 
 	search.exposed = True	
