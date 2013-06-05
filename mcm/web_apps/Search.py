dfcb3be4b99c38378772deecb2df765d2785eae6
PdmV Service <pdmv.service@cern.ch>
2013-06-05 14:09:32 +0200

    move multiple search to database class. mechanism of creating views on new search combination

--- mcm/web_apps/Search.py
+++ mcm/web_apps/Search.py
@@ -19,7 +19,7 @@ class Search(object):
 
 		
 	def run_query(self):
-		def unique_res(query_result):
+		"""def unique_res(query_result):
 			docids = map(lambda doc : doc['_id'] , query_result)
 			docids_s = list(set(docids))
 			if len(docids) != len(docids_s):
@@ -32,12 +32,13 @@ class Search(object):
 						return_dict.remove(doc)		
 				return return_dict
 			return query_result
-
+		"""
 		if len(self.query_list):
-			results_list=[]
+			results_list= self.db.queries( self.query_list )
+			"""
 			##make each query separately and retrieve only the doc with counting == len(query_list)
 			for (i,query) in enumerate(self.query_list):
-				res = self.db.query(query, page_num=-1)
+				res = self.db.query(query, page_num=self.page if len(self.query_list)==1 else -1)
 				query_result = unique_res( map(lambda r : r['value'], res) )
 				if i!=0:
 					## get only the one already in the intersection
@@ -45,6 +46,7 @@ class Search(object):
 					results_list = filter(lambda doc : doc['_id'] in id_list, query_result)
 				else:
 					results_list= query_result
+			"""
 			results = { 'results' : results_list}
 			
 			final = simplejson.dumps(results)
@@ -56,7 +58,8 @@ class Search(object):
 				res = self.db.get_all(page_num=self.page)
 			else:
 				res = self.db.query(self.query, page_num=self.page)
-			query_result = unique_res( map(lambda r : r['value'], res) )
+			
+			query_result = self.db.unique_res( map(lambda r : r['value'], res) )
 			results['results'] = query_result
 			final = simplejson.dumps(results)
 			return final
@@ -81,12 +84,69 @@ class Search(object):
 		odb=database(db_name)
 		design = odb.get('_design/%s'%(db_name))
 		allowed_key_search = design['views'].keys()
-		for key in allowed_key_search:
+
+		vetoed_keys = []
+		for (view,f) in design['views'].items():
+			if 'for(' in f['map']:
+				vetoed_keys.append( view )
+		allowed_key_search.sort()
+		multiple_view=[]
+		#### 
+		## to switch on/off the view creation on the fly
+		simple_search=False
+		####
+		for key in filter (lambda s : '-' not in s, allowed_key_search):
 			if key in args:
-				query_list.append('%s==%s'%(key,args[key]))
+				if key in vetoed_keys or simple_search:
+					query_list.append('%s==%s'%(key,args[key]))
+				else:
+					multiple_view.append( (key, '"'+args[key]+'"') )
 				args.pop(key)
 
+		if len(multiple_view)>1:
+			multiple_search = '-'.join( map( lambda p:p[0], multiple_view))
+			## faster query with multiple keys
+			if not multiple_search in allowed_key_search:
+				## try harder to find it
+				really_not_there=True
+				m_set = set( map( lambda p:p[0], multiple_view)  )
+				for key in filter (lambda s : '-' in s, allowed_key_search):
+					## parse all composite view
+					if set(key.split('-')) == m_set:
+						#we found one that has the same search in absolute, just the order is different
+						# then re-order multiple_view so as to map to the existing view
+						new_multiple_view = []
+						for sv in key.split('-'):
+							new_multiple_view.append( filter( lambda e : e[0]==sv, multiple_view) [0] )
+						multiple_view = new_multiple_view
+						multiple_search = '-'.join( map( lambda p:p[0], multiple_view))
+						really_not_there=False
+						break
+				if really_not_there:
+				        #tempatively add the view to the design
+					new_func = "function(doc){ emit([%s], doc);}"%( ','.join(map( lambda k: "doc.%s"%(k), map( lambda p:p[0], multiple_view) )))
+					design['views'] [ multiple_search ] = { "map" : new_func }
+					saved = odb.update( design )
+		                        ##### NOTE ####
+				        ## the query that will follow will be super slow because the view needs to be re-build
+				
+				
+			m_query = '%s==[%s]'%(multiple_search,
+					    ','.join( map( lambda p:p[1], multiple_view))
+					    )
+			query_list.append( m_query )	       
+			#query_list =[]
+		elif len(multiple_view)==1:
+			m_query = '%s==%s'%( multiple_view[0][0], multiple_view[0][1])
+			query_list.append( m_query )
+
+		#revert to simple query for one query only
+		if len(query_list)==1:
+			query=query_list[0]
+			query_list=[]
+
 		if len(args):
+			## check whether the key is actually a member of the object in db and put back the view in the odb design
 			return simplejson.dumps(args)
 			#return simplejson.dumps(design['views'].keys())
 		return self.search(db_name, query, page, query_list)
