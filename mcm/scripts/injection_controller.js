1447302290d9e7330b7ab9b0d8f68e956cf02265
PdmV Service <pdmv.service@cern.ch>
2013-06-19 18:21:45 +0200

    fix for roles role

--- mcm/scripts/injection_controller.js
+++ mcm/scripts/injection_controller.js
@@ -19,7 +19,7 @@ function resultsCtrl($scope, $http, $location,$timeout, $window){
     var promise = $http.get("restapi/users/get_role");
     promise.then(function(data){
       $scope.user.name = data.data.username;
-      $scope.user.role = data.data.roles[0];
+      $scope.user.role = data.data.role;
       $scope.user.roleIndex = parseInt(data.data.role_index);
     },function(data){
       alert("Error getting user information. Error: "+data.status);
