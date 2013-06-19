45f248d4300f12891bb37fc1d29b088a807fc900
PdmV Service <pdmv.service@cern.ch>
2013-06-19 16:42:45 +0200

    shown in cookies

--- mcm/scripts/users_controller.js
+++ mcm/scripts/users_controller.js
@@ -9,13 +9,12 @@ function resultsCtrl($scope, $http, $location, $window){
   $scope.update = [];
 
   $scope.show_well = false;
-
   if ($location.search()["db_name"] === undefined){
     $scope.dbName = "users";
   }else{
     $scope.dbName = $location.search()["db_name"];
   }
-
+  
   if($location.search()["page"] === undefined){
     page = 0;
     $location.search("page", 0);
@@ -98,8 +97,16 @@ function resultsCtrl($scope, $http, $location, $window){
             $scope.defaults.push({text:v[0].toUpperCase()+v.substring(1).replace(/\_/g,' '), select:false, db_name:v});
           }
         });
+        var shown = "";
+        if ($.cookie($scope.dbName+"shown") !== undefined){
+          shown = $.cookie($scope.dbName+"shown");
+          $location.search("shown", shown);
+        }
         if ($location.search()["shown"] !== undefined){
-          binary_shown = parseInt($location.search()["shown"]).toString(2).split('').reverse().join(''); //make a binary string interpretation of shown number
+          shown = $location.search()["shown"]
+        }
+        if (shown != ""){
+          binary_shown = parseInt(shown).toString(2).split('').reverse().join(''); //make a binary string interpretation of shown number
           _.each($scope.defaults, function(column){
             column_index = $scope.defaults.indexOf(column);
             binary_bit = binary_shown.charAt(column_index);
@@ -176,6 +183,24 @@ function resultsCtrl($scope, $http, $location, $window){
       $scope.update["success"] = false;
       $scope.update["fail"] = true;
       $scope.update["status_code"] = data.status;
+    });
+  };
+  $scope.saveCookie = function(){
+    var cookie_name = $scope.dbName+"shown";
+    if($location.search()["shown"]){
+      $.cookie(cookie_name, $location.search()["shown"], { expires: 7000 })
+    }
+  };
+  $scope.useCookie = function(){
+    var cookie_name = $scope.dbName+"shown";
+    var shown = $.cookie(cookie_name);
+    binary_shown = parseInt(shown).toString(2).split('').reverse().join('');
+    _.each($scope.defaults, function(elem,index){
+      if (binary_shown.charAt(index) == 1){
+        elem.select = true;
+      }else{
+        elem.select = false;
+      }
     });
   };
 };
