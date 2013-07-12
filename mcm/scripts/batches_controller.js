a5c5a7019d4b5fe9f2d799ec02e0ce8af07cdf77
PdmV Service <pdmv.service@cern.ch>
2013-07-12 15:30:06 +0200

    changed history view

--- mcm/scripts/batches_controller.js
+++ mcm/scripts/batches_controller.js
@@ -255,15 +255,17 @@ testApp.directive("customHistory", function(){
     '      <thead>'+
     '        <tr>'+
     '          <th style="padding: 0px;">Action</th>'+
-    '          <th style="padding: 0px;">Message</th>'+
+	//    '          <th style="padding: 0px;">Message</th>'+
     '          <th style="padding: 0px;">Date</th>'+
     '          <th style="padding: 0px;">User</th>'+
+    '          <th style="padding: 0px;">Step</th>'+
+
     '        </tr>'+
     '      </thead>'+
     '      <tbody>'+
     '        <tr ng-repeat="elem in show_info">'+
     '          <td style="padding: 0px;">{{elem.action}}</td>'+
-    '          <td style="padding: 0px;"><a rel="tooltip" title={{elem.message}}><i class="icon-info-sign"></i></a></td>'+
+	//    '          <td style="padding: 0px;"><a rel="tooltip" title={{elem.message}}><i class="icon-info-sign"></i></a></td>'+
     '          <td style="padding: 0px;">{{elem.updater.submission_date}}</td>'+
     '          <td style="padding: 0px;">'+
     '              <div ng-switch="elem.updater.author_name">'+
@@ -271,6 +273,7 @@ testApp.directive("customHistory", function(){
     '                <div ng-switch-default>{{elem.updater.author_name}}</div>'+
     '              </div>'+
     '          </td>'+
+    '          <td style="padding: 0px;">{{elem.step}}</td>'+
     '        </tr>'+
     '      </tbody>'+
     '    </table>'+
