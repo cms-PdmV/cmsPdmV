60ae3fb2a94136a3907d82ec0b737db5ecaf32cc
root <root@preptest.cern.ch>
2013-07-16 16:33:50 +0200

    thread pool alpha implementation

--- mcm/tools/handler.py
+++ mcm/tools/handler.py
@@ -1,15 +1,56 @@
 import time
 import os
+import traceback
 
-from threading import Thread
+from threading import Thread, Lock
 
 import logging
 from tools.logger import logger as logfactory, prep2_formatter
 from tools.locator import locator
 from tools.batch_control import batch_control
 from couchdb_layer.prep_database import database
+from collections import Iterable
 #from json_layer.request import request
 from tools.installer import installer
+
+
+class PoolOfHandlers(Thread):
+    """
+    Class used for instantiating and taking care of running a number of handlers in parallel. It provides them
+    with lock for protection of concurrently-vulnerable parts of program (e.g. database access).
+    """
+
+    logger = logfactory('mcm')
+
+    def __init__(self, handler_class, arguments):
+        """
+        handler_class is used to instantiate the objects.
+
+        arguments parameter is a list of arguments (dictionnaries) passed to the handler_class' constructor method.
+        """
+        Thread.__init__(self)
+        try:
+            self._handlers_list = []
+            self._lock = Lock()
+            for arg in arguments:
+                if not isinstance(arg, dict):  # if arg is e.g. list
+                    raise TypeError("Arguments should be a list of dictionaries")
+                arg['lock'] = self._lock
+                self._handlers_list.append(handler_class(**arg))
+            self.logger.log("Instantiated %s handlers %s in pool" % (len(arguments), handler_class.__name__))
+        except:
+            self.logger.error('Failed to instantiate handlers \n %s' % (traceback.format_exc()))
+
+    def run(self):
+        """
+        Starts the handlers from pool.
+        """
+        self.logger.log("Starting %s handlers" % (len(self._handlers_list)))
+        for handler_object in self._handlers_list:
+            handler_object.start()
+        for handler_object in self._handlers_list:
+            handler_object.join()
+
 
 class handler(Thread):
     """
@@ -17,10 +58,15 @@ class handler(Thread):
     """
     logger = logfactory('mcm')
     hname = '' # handler's name
+    lock = None
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         Thread.__init__(self)
         self.res = []
+        if 'lock' not in kwargs:
+            self.lock = Lock()
+        else:
+            self.lock = kwargs['lock']
 
     def run(self):
         try:
