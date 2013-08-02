b5da68ff980f03ae155bc2e1efb5552d24cb9deb
PdmV Service <pdmv.service@cern.ch>
2013-08-02 16:24:41 +0200

    tabulation. caching of user role for perf improvement.

--- mcm/rest_api/UserActions.py
+++ mcm/rest_api/UserActions.py
@@ -1,106 +1,113 @@
 #!/usr/bin/env python
 
 import cherrypy
-from json import loads,dumps
+from json import loads, dumps
 from RestAPIMethod import RESTResource
 from couchdb_layer.prep_database import database
 
-class GetUserRole(RESTResource):
-	def __init__(self):
-		self.authenticator.set_limit(0)
 
-	def GET(self):
-		"""
+class GetUserRole(RESTResource):
+    def __init__(self):
+        self.authenticator.set_limit(0)
+
+    def GET(self):
+        """
 		Retrieve the role (string) of the current user
 		"""
-		return self.get_user_role()
+        return self.get_user_role()
 
-	def get_user_role(self):
-                user = cherrypy.request.headers['ADFS-LOGIN']
-		communicationLine=None
-		if 'REMOTE-USER' in cherrypy.request.headers:
-			communicationLine = cherrypy.request.headers['REMOTE-USER']
-		role = self.authenticator.get_user_role(user,email=communicationLine)
-		role_index = self.authenticator.get_roles().index(role)
-		return dumps({'username':user, 'role':role, 'role_index':  role_index})
+    def get_user_role(self):
+        user = cherrypy.request.headers['ADFS-LOGIN']
+        communicationLine = None
+        if 'REMOTE-USER' in cherrypy.request.headers:
+            communicationLine = cherrypy.request.headers['REMOTE-USER']
+        role = self.authenticator.get_user_role(user, email=communicationLine)
+        role_index = self.authenticator.get_roles().index(role)
+        return dumps({'username': user, 'role': role, 'role_index': role_index})
+
 
 class GetAllRoles(RESTResource):
-	def __init__(self):
-	    self.authenticator.set_limit(0)
+    def __init__(self):
+        self.authenticator.set_limit(0)
 
-	def GET(self):
-		"""
+    def GET(self):
+        """
 		Retrieve the list of possible roles 
 		"""
-		return self.get_All_roles()
+        return self.get_All_roles()
 
-	def get_All_roles(self):
-   	    role = self.authenticator.get_roles()
-	    return dumps(role)
+    def get_All_roles(self):
+        role = self.authenticator.get_roles()
+        return dumps(role)
+
 
 class GetUserPWG(RESTResource):
     def __init__(self):
-            self.db_name = 'users'
-            self.db = database(self.db_name)
+        self.db_name = 'users'
+        self.db = database(self.db_name)
 
-    def GET(self , *args):
-	    """
+    def GET(self, *args):
+        """
 	    Retrieve the pwg of the provided user
 	    """
-	    ## this could be a specific database in couch, to hold the list, with maybe some added information about whatever the group does...
-	    all_pwgs=['BPH', 'B2G', 'BTV', 'EGM', 'EWK', 'EXO', 'FWD', 'HIG', 'HIN', 'JME', 'MUO', 'QCD', 'SUS', 'TAU', 'TRK', 'TOP','TSG','SMP']
-	    all_pwgs.sort()
-	    if len(args)==0:
-		    return dumps({"results" : all_pwgs})
-	    user_name = args[0]
-	    if self.db.document_exists( user_name ):
-		    user = self.db.get( args[0] )
-		    if user['role'] in ['production_manager','administrator','generator_convener']:
-			    return dumps({"results" : all_pwgs})
-		    else:
-			    return dumps({"results" : user['pwg']})
-	    else:
-		    return dumps({"results" : []})
+        ## this could be a specific database in couch, to hold the list, with maybe some added information about whatever the group does...
+        all_pwgs = ['BPH', 'B2G', 'BTV', 'EGM', 'EWK', 'EXO', 'FWD', 'HIG', 'HIN', 'JME', 'MUO', 'QCD', 'SUS', 'TAU',
+                    'TRK', 'TOP', 'TSG', 'SMP']
+        all_pwgs.sort()
+        if len(args) == 0:
+            return dumps({"results": all_pwgs})
+        user_name = args[0]
+        if self.db.document_exists(user_name):
+            user = self.db.get(args[0])
+            if user['role'] in ['production_manager', 'administrator', 'generator_convener']:
+                return dumps({"results": all_pwgs})
+            else:
+                return dumps({"results": user['pwg']})
+        else:
+            return dumps({"results": []})
 
 
 class GetAllUsers(RESTResource):
     def __init__(self):
-	    self.db_name = 'users'
-	    self.db = database(self.db_name)
+        self.db_name = 'users'
+        self.db = database(self.db_name)
 
     def GET(self):
-	    """
+        """
 	    Retrieve the db content for all users
 	    """
-	    return self.get_Users()
+        return self.get_Users()
 
     def get_Users(self):
-   	    # roles = self.authenticator.get_roles()
-   	    return dumps({"results":self.db.get_all()})
-	    # return dumps("test")
+        # roles = self.authenticator.get_roles()
+        return dumps({"results": self.db.get_all()})
+        # return dumps("test")
+
 
 class GetUser(RESTResource):
     def __init__(self):
-	    self.db_name = 'users'
-	    self.db = database(self.db_name)
-	    
+        self.db_name = 'users'
+        self.db = database(self.db_name)
+
     def GET(self, *args):
-	    """
+        """
 	    Retrieve the information about a provided user
 	    """
-	    return dumps({"results":self.db.get( args[0])})
+        return dumps({"results": self.db.get(args[0])})
+
 
 class SaveUser(RESTResource):
     def __init__(self):
-	    self.db_name = 'users'
-	    self.db = database(self.db_name)
-	    self.access_limit = 3
+        self.db_name = 'users'
+        self.db = database(self.db_name)
+        self.access_limit = 3
 
     def PUT(self):
-	    """
+        """
 	    Save the information about a given user
 	    """
-	    return dumps({"results":self.db.save( loads(cherrypy.request.body.read().strip()))})
+        return dumps({"results": self.db.save(loads(cherrypy.request.body.read().strip()))})
+
 
 class AddRole(RESTResource):
     def __init__(self):
@@ -111,8 +118,8 @@ class AddRole(RESTResource):
 
     def add_user(self):
         username = cherrypy.request.headers['ADFS-LOGIN']
-	if self.db.document_exists( username ):
-		return dumps({"results":False})
+        if self.db.document_exists(username):
+            return dumps({"results": False})
         email = cherrypy.request.headers['REMOTE-USER']
         role = self.authenticator.get_roles()[0]
         user = {}
@@ -120,49 +127,53 @@ class AddRole(RESTResource):
         user["username"] = username
         user["email"] = email
         #user["roles"] = [role]
-	user["role"] = role
-	user['pwg'] = []
+        user["role"] = role
+        user['pwg'] = []
         # save to db
         if not self.db.save(user):
             self.logger.error('Could not save object to database')
-            return dumps({"results":False})
-        return dumps({"results":True})
+            return dumps({"results": False})
+        return dumps({"results": True})
 
     def GET(self):
-	    """
+        """
 	    Add the current user to the user database if not already
 	    """
-	    return self.add_user()
+        return self.add_user()
+
 
 class ChangeRole(RESTResource):
     def __init__(self):
         self.db_name = 'users'
         self.db = database(self.db_name)
         self.all_roles = self.authenticator.get_roles()
-	self.access_limit = 3
-	
+        self.access_limit = 3
+
     def change_role(self, username, action):
         doc = self.db.get(username)
         current_user = self.db.get(cherrypy.request.headers['ADFS-LOGIN'])
         current_role = doc["role"]
         if action == '-1':
             if current_role != 'user': #if not the lowest role -> then him lower himself
-                doc["role"] = self.all_roles[self.all_roles.index(current_role)-1]
-                return dumps({"results":self.db.update(doc)})
-            return dumps({"results":username+" already is user"}) #else return that hes already a user
+                doc["role"] = self.all_roles[self.all_roles.index(current_role) - 1]
+                self.authenticator.set_user_role(username, doc["role"])
+                return dumps({"results": self.db.update(doc)})
+            return dumps({"results": username + " already is user"}) #else return that hes already a user
         if action == '1':
             if current_user["role"] != "administrator":
-                return dumps({"results":"Only administrators can upgrade roles"})
-            if len(self.all_roles) != self.all_roles.index(current_role)+1: #if current role is not the top one
-                doc["role"] = self.all_roles[self.all_roles.index(current_role)+1]
-                return dumps({"results":self.db.update(doc)})
-            return dumps({"results":username+" already has top role"})
-        return dumps({"results":"Failed to update user: "+username+" role"})
+                return dumps({"results": "Only administrators can upgrade roles"})
+            if len(self.all_roles) != self.all_roles.index(current_role) + 1: #if current role is not the top one
+                doc["role"] = self.all_roles[self.all_roles.index(current_role) + 1]
+                self.authenticator.set_user_role(username, doc["role"])
+                return dumps({"results": self.db.update(doc)})
+            return dumps({"results": username + " already has top role"})
+        return dumps({"results": "Failed to update user: " + username + " role"})
+
     def GET(self, *args):
         """
 	Increase /1 or decrease /-1 the given user role by one unit of role
 	"""
         if not args:
             self.logger.error("No Arguments were given")
-            return dumps({"results":'Error: No arguments were given'})
-        return self.change_role(args[0],args[1])
+            return dumps({"results": 'Error: No arguments were given'})
+        return self.change_role(args[0], args[1])
--- mcm/tools/authenticator.py
+++ mcm/tools/authenticator.py
@@ -1,107 +1,118 @@
 #!/usr/bin/env python
 
 from couchdb_layer.prep_database import database
+from tools.locker import locker
+
 
 class authenticator:
-	def __init__(self, limit=0):
-		# roles list is a list of valid roles for a page
-		self.__roles = ['user', 'generator_contact', 'generator_convener', 'production_manager', 'administrator']
-		self.__db = database('users')
+    def __init__(self, limit=0):
+        # roles list is a list of valid roles for a page
+        self.__roles = ['user', 'generator_contact', 'generator_convener', 'production_manager', 'administrator']
+        self.__db = database('users')
+        self.__users_roles = dict()
 
-		# limit is the numeric representation of a base role that cane
-		# access a specific page
-		self.__limit = 0
+        # limit is the numeric representation of a base role that cane
+        # access a specific page
+        self.__limit = 0
 
-                # base role is the minimum requirement of a user to have in
-                # order to access a specific page
-		self.__base_role = ''
+        # base role is the minimum requirement of a user to have in
+        # order to access a specific page
+        self.__base_role = ''
 
-		self.set_limit(limit)
-	
-	# get the roles that are registered to a specific username
-	def get_user_role(self, username,email=None):
-		if not self.__db.document_exists(username):
-			return self.__roles[0]
-		user=self.__db.get(username)
-		if email and ('email' not in user or user['email']!=email):
-			user['email']=email
-			self.__db.update(user)
-		role=None
-		while not role:
-			try:
-				role = user['role']
-			except:
-				## how to print an error from here ?
-				user=self.__db.get(username)
-				pass
-		return user['role']
+        self.set_limit(limit)
 
-	def get_user_role_index(self,username,email=None):
-		r=self.get_user_role(username,email)
-		return self.__roles.index(r),r
+    # get the roles that are registered to a specific username
+    def get_user_role(self, username, email=None):
+        with locker.lock(username):
+            if username not in self.__users_roles:
+                if not self.__db.document_exists(username):
+                    self.__users_roles[username] = self.__roles[0]
+                else:
+                    user = self.__db.get(username)
+                    if email and ('email' not in user or user['email'] != email):
+                        user['email'] = email
+                        self.__db.update(user)
+                    role = None
+                    while not role:
+                        try:
+                            role = user['role']
+                        except:
+                            ## how to print an error from here ?
+                            user = self.__db.get(username)
+                            pass
+                    self.__users_roles[username] = user['role']
+            return self.__users_roles[username]
 
-	# aux: get the list of __roles
-	def get_roles(self):
-		return self.__roles
-	
-	# aux: get the numeric access limit
-	def get_limit(self):
-		return self.__limit
+    def get_user_role_index(self, username, email=None):
+        r = self.get_user_role(username, email)
+        return self.__roles.index(r), r
 
-	def get_base_role(self):
-		return self.__base_role
-	
-	# aux: set the list of valid roles
-	def set_roles(self, roles=[]):
-		if not roles:
-			return
-		self.__roles = roles
-	
-	def set_limit(self, limit=0):
-                if limit < 0:
-                        raise ValueError('Access Limit provided is invalid: '+str(self.__limit))
+    # aux: get the list of __roles
+    def get_roles(self):
+        return self.__roles
 
-                if limit >= len(self.__roles):
-                        raise ValueError('Access Limit provided is illegal: '+str(self.__limit))
+    # aux: get the numeric access limit
+    def get_limit(self):
+        return self.__limit
 
-		self.__limit = limit
+    def get_base_role(self):
+        return self.__base_role
 
-                self.__base_role = self.__roles[self.__limit]
-	
-	# returns True, if a user matches the base role or higher
-	# returns False, otherwise.
-	def can_access(self, username):
-		role = self.get_user_role(username)
-		
-		if self.__base_role == role:
-			return True
-		
-		# if the user does not match the given role, then
-		# maybe he has higher access rights.
-		if role not in self.__roles:
-			###exception actually
-			raise ValueError('role %s is not recognized'%( role ))
-		if self.__roles.index(role) >= self.__limit:
-			return True
-		return False
+    # aux: set the list of valid roles
+    def set_roles(self, roles=[]):
+        if not roles:
+            return
+        self.__roles = roles
 
-	def get_login_box(self, username):
-		res = '<div id="login_box" style="float: right; display: block;"> '
-		res += str(username)
-		
-		role = self.get_user_role(username)
-		
-		res += '\t(\tRole: ' + str(role)
-		res += "\t)\t<a href='https://login.cern.ch/adfs/ls/?wa=wsignout1.0' style='float: right'>logout</a>"
-		res += "</div>"
-		return res
-	
-	@classmethod
-	def user_has_access(cls, username, limit):
-		auth = cls(limit)
-		try:
-			flag = auth.can_access(username)
-		except ValueError as ex:
-			print 'Error: '+str(ex)
-			return False
-		return flag
+    def set_user_role(self, username, role):
+        with locker.lock(username):
+            self.__users_roles[username] = role
+
+    def set_limit(self, limit=0):
+        if limit < 0:
+            raise ValueError('Access Limit provided is invalid: ' + str(self.__limit))
+
+        if limit >= len(self.__roles):
+            raise ValueError('Access Limit provided is illegal: ' + str(self.__limit))
+
+        self.__limit = limit
+
+        self.__base_role = self.__roles[self.__limit]
+
+    # returns True, if a user matches the base role or higher
+    # returns False, otherwise.
+    def can_access(self, username):
+        role = self.get_user_role(username)
+
+        if self.__base_role == role:
+            return True
+
+        # if the user does not match the given role, then
+        # maybe he has higher access rights.
+        if role not in self.__roles:
+            ###exception actually
+            raise ValueError('role %s is not recognized' % ( role ))
+        if self.__roles.index(role) >= self.__limit:
+            return True
+        return False
+
+    def get_login_box(self, username):
+        res = '<div id="login_box" style="float: right; display: block;"> '
+        res += str(username)
+
+        role = self.get_user_role(username)
+
+        res += '\t(\tRole: ' + str(role)
+        res += "\t)\t<a href='https://login.cern.ch/adfs/ls/?wa=wsignout1.0' style='float: right'>logout</a>"
+        res += "</div>"
+        return res
+
+    @classmethod
+    def user_has_access(cls, username, limit):
+        auth = cls(limit)
+        try:
+            flag = auth.can_access(username)
+        except ValueError as ex:
+            print 'Error: ' + str(ex)
+            return False
+        return flag
