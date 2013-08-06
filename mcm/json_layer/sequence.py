fc183801d73aec3ee1ee6942e76a7c98dc9ba07f
PdmV Service <pdmv.service@cern.ch>
2013-08-06 18:42:05 +0200

    fix for himix specification

--- mcm/json_layer/sequence.py
+++ mcm/json_layer/sequence.py
@@ -26,6 +26,7 @@ class sequence(json_base):
             'donotDropOnInput':'',
             'restoreRNDSeeds':'',
             'slhc':'',
+            'himix':False,
             'inputEventContent':'',
             'gflash':'',
             'geometry':'',
