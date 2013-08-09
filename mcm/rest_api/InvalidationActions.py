7920e3dde4c55e9f3257f603086eb71ad5d376c4
PdmV Service <pdmv.service@cern.ch>
2013-08-09 18:25:25 +0200

    better handling of invalidation and enabling announcement to HN

--- mcm/rest_api/InvalidationActions.py
+++ mcm/rest_api/InvalidationActions.py
@@ -5,133 +5,144 @@ import sys
 import traceback
 import string
 import time
+import itertools
 from math import sqrt
-from json import loads,dumps
+from json import loads, dumps
 from couchdb_layer.prep_database import database
 from RestAPIMethod import RESTResource
 from tools.communicator import communicator
 from tools.locator import locator
+from json_layer.invalidation import invalidation
+
 
 class Invalidate(RESTResource):
+
     def __init__(self):
-        self.access_limit =4
+        self.access_limit = 4
+
     def GET(self, *args):
         """
         Operate the invalidation of a given document
         """
         if not len(args):
-            return dumps({'results':False,'message':'not id has been provided'})
+            return dumps({'results': False, 'message': 'not id has been provided'})
 
         docid = args[0]
         invalidations = database('invalidations')
         if not invalidations.document_exists(docid):
-            return dumps({'results':False,'message':'%s does not exists'%( docid)})
-        
-        invalid = invalidations.get(docid)
-        if invalid['type'] == 'request':
-            return dumps({'results':False,'message':'Not implemented to invalidate requests'})
-        elif invalid['type'] == 'dataset':
-            return dumps({'results':False,'message':'Not implemented to invalidate datasets'})
+            return dumps({'results': False, 'message': '%s does not exists' % docid})
+
+        invalid = invalidation(invalidations.get(docid))
+        if invalid.get_attribute('type') in ['request', 'dataset']:
+            return dumps({'results': False,
+                          'message': 'Not implemented to invalidate {0}s'.format(invalid.get_attribute('type'))})
         else:
-            return dumps({'results':False,'message':'Type %s not recognized'%( invalid['type'])})
+            return dumps({'results': False, 'message': 'Type {0} not recognized'.format(invalid.get_attribute('type'))})
 
 
 class SetStatus(RESTResource):
+
     def __init__(self):
-        self.access_limit =4
+        self.access_limit = 4
+
     def GET(self, *args):
         """
         Set the status of an invalidation to the next status
         """
         if not len(args):
-            return dumps({'results':False,'message':'not id has been provided'})
+            return dumps({'results': False, 'message': 'not id has been provided'})
 
         docid = args[0]
         invalidations = database('invalidations')
         if not invalidations.document_exists(docid):
-            return dumps({'results':False,'message':'%s does not exists'%( docid)})
-        
-        invalid = invalidations.get(docid)
-        invalid['status']=done
-        invalidations.update( invalid )
-        return dumps({'results':True})
+            return dumps({'results': False, 'message': '%s does not exists' % ( docid)})
+
+        invalid = invalidation(invalidations.get(docid))
+        invalid.set_next_status()
+        invalidations.update(invalid.json())
+        return dumps({'results': True})
 
 
-class InspectInvalidation(RESTResource):  
+class InspectInvalidation(RESTResource):
+
     def __init__(self):
-        self.access_limit =3
+        self.access_limit = 3
 
     def GET(self, *args):
         """
         Goes through invalidation documents and display (/) and announce them to data ops (/announce)
         """
 
-        announce=False
-        if len(args)!=0:
-            if args[0]=='announce':
-                announce=True
-        
-        idb = database('invalidations')
-        
-        r_to_be_rejected = idb.queries(['status==new','type==request'])
-        ds_to_be_invalidated = idb.queries(['status==new','type==dataset'])
-        
-        l_type= locator()
+        announce = False
+        if len(args) != 0:
+            if args[0] == 'announce':
+                announce = True
 
-        html='<html><body>\n'
-        html+='Requests to be aborted/rejected <br>\n'
-        html+='<ul>\n'
+        idb = database('invalidations')
+        r_to_be_rejected = map(invalidation, idb.queries(['status==new', 'type==request']))
+        ds_to_be_invalidated = map(invalidation, idb.queries(['status==new', 'type==dataset']))
+        l_type = locator()
+
+        def add_prepid(invalid, html):
+            if 'prepid' in invalid.json():
+                html += 'for request <a href=%srequests?prepid=%s> %s </a>' % (
+                    l_type.baseurl(), invalid.get_attribute('prepid'), invalid.get_attribute('prepid'))
+
+        def print_invalidations(invalids, text):
+            for invalid in invalids:
+                text += ' %s\n' % (invalid.get_attribute('object'))
+
+        html = '<html><body>\n'
+        html += 'Requests to be aborted/rejected <br>\n'
+        html += '<ul>\n'
         for r in r_to_be_rejected:
-            html+='<li> <a href=%sreqmgr/view/details/%s> %s </a>'%(l_type.cmsweburl() , r['object'] ,r['object'] )
-            if 'prepid' in r:
-                html+='for request <a href=%srequests?prepid=%s> %s </a>'%(l_type.baseurl(), r['prepid'], r['prepid'])
-            html+='</li>\n'
-        html+='</ul>\n'
-        html+='Datasets be invalidated <br>\n'
-        html+='<ul>\n'
+            html += '<li> <a href=%sreqmgr/view/details/%s> %s </a>' % (l_type.cmsweburl(), r.get_attribute('object'), r.get_attribute('object') )
+            add_prepid(r, html)
+            html += '</li>\n'
+        html += '</ul>\n'
+        html += 'Datasets to be invalidated <br>\n'
+        html += '<ul>\n'
         for ds in ds_to_be_invalidated:
-            html+='<li> %s '%( ds['object'] )
-            if 'prepid' in ds:
-                html+='for request <a href=%srequests?prepid=%s> %s </a>'%(l_type.baseurl(), ds['prepid'], ds['prepid'])
-            html+='</li>\n'
-        html+='</ul>\n' 
-        html+='</html></body>\n'
+            html += '<li> %s ' % ( ds.get_attribute('object') )
+            add_prepid(ds, html)
+            html += '</li>\n'
+        html += '</ul>\n'
+        html += '</html></body>\n'
 
         if announce:
-            text ='Dear Data Operation Team,\n\n'
-            text+='please reject or abort the following requests:\n'
-            for r in r_to_be_rejected:
-                text+=' %s\n'%(r['object'])
-            text+='\n'
-            text+='Please invalidate the following datasets:\n'
-            for ds in ds_to_be_invalidated:
-                text+=' %s\n'%(ds['object'])
-            text+='\n'
-            text+'as a consequence of requests being reset.\n'
+            text = 'Dear Data Operation Team,\n\n'
+            text += 'please reject or abort the following requests:\n'
+            print_invalidations(r_to_be_rejected, text)
+            text += '\nPlease invalidate the following datasets:\n'
+            print_invalidations(ds_to_be_invalidated, text)
+            text += '\nas a consequence of requests being reset.\n'
             com = communicator()
-
 
             to_who = ['pdmvserv@cern.ch']
             if not l_type.isDev():
-                to_who.append( 'hn-cms-dataopsrequests@cern.ch' )
+                to_who.append('hn-cms-dataopsrequests@cern.ch')
 
-            ## to be replace
+            # set sender as current user, so the message can be sent to hs-cms-dataopsrequests@cern.ch
+            try:
+                elem = (r_to_be_rejected + ds_to_be_invalidated)[0]
+                sender = elem.current_user_email
+            except IndexError:
+                sender = None
+
             com.sendMail(to_who,
                          'Request and Datasets to be Invalidated',
-                         text)
-            
-            for r in r_to_be_rejected:
-                r['status']='announced'
-                idb.update(r)
-            for ds in ds_to_be_invalidated:
-                ds['status']='announced'
-                idb.update(ds)
+                         text,
+                         sender)
 
+            for to_announce in itertools.chain(r_to_be_rejected, ds_to_be_invalidated):
+                to_announce.set_announced()
+                idb.update(to_announce.json())
 
         return html
 
 
 class GetInvalidation(RESTResource):
+
     def __init__(self):
         self.access_limit = 4
         self.db = database('invalidations')
