02d0ef7dc91a9a2e2bfe2a18af2d7deba0ec06a5
PdmV Service <pdmv.service@cern.ch>
2013-08-26 13:09:01 +0200

    not always loading news.

--- mcm/scripts/main_controller.js
+++ mcm/scripts/main_controller.js
@@ -24,31 +24,46 @@ function mainCtrl($scope, $http, $location, $window){
 // Endo of user info request
 
 // GET all news
-  var promise = $http.get("restapi/news/getall/5");
-  promise.then(function(data){
-    $scope.news = data.data;
-    var new_marquee = document.createElement('marquee');
-    var news_banner = document.getElementById("news_banner");
-    if(news_banner){
-      new_marquee.setAttribute('direction','left');
-      new_marquee.setAttribute('behavior','scroll');
-      var sorted_news = _.sortBy($scope.news, function(elem){ //sort news array by date
-        return elem.date;
-      });
-      //changed in the rest api directly
-      sorted_news.reverse(); //lets reverse it so newest new is in beggining of array
-  //    sorted_news = sorted_news.splice(0,5); //take only 5 newest and best news
-      _.each(sorted_news, function(v){
-        new_new = "<span> <i class='icon-globe'></i><b>"+v.subject+"</b>  <i>"+v.date+" </i></span>";
-          new_marquee.innerHTML += new_new;
-      });
-      news_banner.appendChild(new_marquee);
-      news_banner.appendChild(new_marquee);
-    }
-  },function(data){
-    alert("Error getting news. Error: "+data.status);
-  });
+  $scope.getNews = function(){ //we check for wich page to get news -> home page gets news all the time
+    var pages_not_to_get_news = ["chained_campaigns","flows","actions","requests","chained_requests","batch","dashboard","users"];
+    var return_info = true;
+    _.each(pages_not_to_get_news, function(elem){
+      if($location.path().indexOf(elem) != -1)
+      {
+        return_info = false;
+      }
+    });
+      return return_info;
+  };
+  if ($scope.getNews()){
+    var promise = $http.get("restapi/news/getall/5");
+    promise.then(function(data){
+      $scope.news = data.data;
+      var new_marquee = document.createElement('marquee');
+      var news_banner = document.getElementById("news_banner");
+      if(news_banner){
+        new_marquee.setAttribute('direction','left');
+        new_marquee.setAttribute('behavior','scroll');
+        var sorted_news = _.sortBy($scope.news, function(elem){ //sort news array by date
+          return elem.date;
+        });
+        //changed in the rest api directly
+        sorted_news.reverse(); //lets reverse it so newest new is in beggining of array
+    //    sorted_news = sorted_news.splice(0,5); //take only 5 newest and best news
+        _.each(sorted_news, function(v){
+          new_new = "<span> <i class='icon-globe'></i><b>"+v.subject+"</b>  <i>"+v.date+" </i></span>";
+            new_marquee.innerHTML += new_new;
+        });
+        news_banner.appendChild(new_marquee);
+        news_banner.appendChild(new_marquee);
+      }
+    },function(data){
+      alert("Error getting news. Error: "+data.status);
+    });
+  }
 // Endo of news!
+
+
 
   $scope.isDevMachine = function(){
     is_dev = $location.absUrl().indexOf("dev") != -1;
@@ -122,6 +137,7 @@ testApp.directive('ddlFileReader', function($http,$rootScope) {
         template: '<input type="file" class="input" />'
     }
 });
+
 /*
 Angular-UI panes/tab directive with local customisation
 http://angular-ui.github.io/bootstrap/
@@ -132,11 +148,14 @@ testApp.controller('TabsController', ['$scope', '$element', function($scope, $el
   this.select = $scope.select = function selectPane(pane) {
     if (pane.selected == true){ //if pane is clicked while open -> close pane to save space
       pane.selected = false;
+      pane.active = false;
     }else{ //else if it was closed-> open clicked pane by closing all and opening the current one
       angular.forEach(panes, function(pane) {
         pane.selected = false;
+        pane.active = false;
       });
       pane.selected = true;
+      pane.active = true;
     }
   };
 
@@ -147,15 +166,15 @@ testApp.controller('TabsController', ['$scope', '$element', function($scope, $el
     panes.push(pane);
   };
 
-  this.removePane = function removePane(pane) { 
+  this.removePane = function removePane(pane) {
     var index = panes.indexOf(pane);
     panes.splice(index, 1);
-    //Select a new pane if removed pane was selected 
+    //Select a new pane if removed pane was selected
     if (pane.selected && panes.length > 0) {
       $scope.select(panes[index < panes.length ? index : index-1]);
     }
   };
-}])
+}]);
 testApp.directive('tabs', function() {
   return {
     restrict: 'EA',
@@ -163,7 +182,7 @@ testApp.directive('tabs', function() {
     scope: {
     },
     controller: 'TabsController',
-    template: 
+    template:
     "<div class=\"tabbable\">\n" +
     "  <ul class=\"nav nav-tabs\">\n" +
     "    <li ng-repeat=\"pane in panes\" ng-class=\"{active:pane.selected}\">\n" +
@@ -174,7 +193,7 @@ testApp.directive('tabs', function() {
     "</div>\n",
     replace: true
   };
-})
+});
 testApp.directive('pane', ['$parse', function($parse) {
   return {
     require: '^tabs',
@@ -182,6 +201,7 @@ testApp.directive('pane', ['$parse', function($parse) {
     transclude: true,
     scope:{
       heading:'@',
+      active:'='
     },
     link: function(scope, element, attrs, tabsCtrl) {
       var getSelected, setSelected;
@@ -211,5 +231,60 @@ testApp.directive('pane', ['$parse', function($parse) {
     },
     template: "<div class=\"tab-pane\" ng-class=\"{active: selected}\" ng-show=\"selected\" ng-transclude></div>\n",
     replace: true
+  };
+}]);
+
+testApp.constant('buttonConfig', {
+    activeClass:'active',
+    toggleEvent:'click'
+  })
+  .directive('btnRadio', ['buttonConfig', function (buttonConfig) {
+  var activeClass = buttonConfig.activeClass || 'active';
+  var toggleEvent = buttonConfig.toggleEvent || 'click';
+  return {
+    require:'ngModel',
+    link:function (scope, element, attrs, ngModelCtrl) {
+      //model -> UI
+      ngModelCtrl.$render = function () {
+        element.toggleClass(activeClass, angular.equals(ngModelCtrl.$modelValue, scope.$eval(attrs.btnRadio)));
+      };
+      //ui->model
+      element.bind(toggleEvent, function () {
+        if (!element.hasClass(activeClass)) {
+          scope.$apply(function () {
+            ngModelCtrl.$setViewValue(scope.$eval(attrs.btnRadio));
+            ngModelCtrl.$render();
+          });
+        }
+      });
+    }
+  };
+}])
+  .directive('btnCheckbox', ['buttonConfig', function (buttonConfig) {
+  var activeClass = buttonConfig.activeClass || 'active';
+  var toggleEvent = buttonConfig.toggleEvent || 'click';
+  return {
+    require:'ngModel',
+    link:function (scope, element, attrs, ngModelCtrl) {
+      function getTrueValue() {
+        var trueValue = scope.$eval(attrs.btnCheckboxTrue);
+        return angular.isDefined(trueValue) ? trueValue : true;
+      }
+      function getFalseValue() {
+        var falseValue = scope.$eval(attrs.btnCheckboxFalse);
+        return angular.isDefined(falseValue) ? falseValue : false;
+      }
+      //model -> UI
+      ngModelCtrl.$render = function () {
+        element.toggleClass(activeClass, angular.equals(ngModelCtrl.$modelValue, getTrueValue()));
+      };
+      //ui->model
+      element.bind(toggleEvent, function () {
+        scope.$apply(function () {
+          ngModelCtrl.$setViewValue(element.hasClass(activeClass) ? getFalseValue() : getTrueValue());
+          ngModelCtrl.$render();
+        });
+      });
+    }
   };
 }]);
