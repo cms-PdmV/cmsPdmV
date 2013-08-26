e88a519e4d0adcb576dba2bc9f2aa8622db77265
PdmV Service <pdmv.service@cern.ch>
2013-08-26 13:29:26 +0200

    logs in dashboard page

--- mcm/rest_api/LogActions.py
+++ mcm/rest_api/LogActions.py
@@ -2,51 +2,52 @@
 
 import cherrypy
 import re
-from json import loads,dumps
+from json import loads, dumps
 from couchdb_layer.prep_database import database
 from RestAPIMethod import RESTResource
 
+
 class ReadInjectionLog(RESTResource):
-	def __init__(self):
-		self.logfile = 'logs/inject.log'
-		self.db_name = 'requests'
-		self.db = database(self.db_name)
+    def __init__(self):
+        self.logfile = 'logs/inject.log'
+        self.db_name = 'requests'
+        self.db = database(self.db_name)
 
-	def GET(self, *args):
+    def GET(self, *args):
+        """
+		Retrieves the last injection log for a given request id
 		"""
-		Retreives the last injection log for a given request id
-		"""
-		if not args:
-			self.logger.error('No arguments were given')
-			return dumps({"results":'Error: No arguments were given'})
-		pid = args[0]
-		nlines=-1
-		if len(args)>1:
-			nlines=int(args[1])
-		return self.read_logs(pid,nlines)
+        if not args:
+            self.logger.error('No arguments were given')
+            return dumps({"results": 'Error: No arguments were given'})
+        pid = args[0]
+        nlines = -1
+        if len(args) > 1:
+            nlines = int(args[1])
+        return self.read_logs(pid, nlines)
 
-	def read_logs(self, pid,nlines):
-		if not self.db.document_exists(pid):
-			self.logger.error('Given prepid "%s" does not exist in the database.' % (pid))
-			return dumps({"results":'Error:Given prepid "%s" does not exist in the database.' % (pid)})
+    def read_logs(self, pid, nlines):
+        if not self.db.document_exists(pid):
+            self.logger.error('Given prepid "%s" does not exist in the database.' % (pid))
+            return dumps({"results": 'Error:Given prepid "%s" does not exist in the database.' % (pid)})
 
-		try:
-			data = open(self.logfile).read()
-		except IOError as ex:
-			self.logger.error('Could not access logs: "%s". Reason: %s' % (self.logfile, ex))
-			return dumps({"results":"Error: Could not access logs."})
+        try:
+            data = open(self.logfile).read()
+        except IOError as ex:
+            self.logger.error('Could not access logs: "%s". Reason: %s' % (self.logfile, ex))
+            return dumps({"results": "Error: Could not access logs."})
 
-		#important = data[data.rindex('## Logger instance retrieved'):]
-		## needs this otherwise, simultaneous submission would be truncated to the last to write ## Logger instance retrieved
-		important = data[data.rindex('[%s] ## Logger instance retrieved'%(pid)):]
-		if not important:
-			raise ValueError('Malformed logs. Could not detect start of injection.')
+        #important = data[data.rindex('## Logger instance retrieved'):]
+        ## needs this otherwise, simultaneous submission would be truncated to the last to write ## Logger instance retrieved
+        important = data[data.rindex('[%s] ## Logger instance retrieved' % (pid)):]
+        if not important:
+            raise ValueError('Malformed logs. Could not detect start of injection.')
 
-		lines = filter(lambda l: pid in l, important.rsplit('\n'))
-		
-		if (nlines>0):
-			lines = lines[-nlines:]
-		res = ''
-		for line in lines:
-			res += '%s<br>' % (line.replace('<breakline>', '<br>'))
-		return res
+        lines = filter(lambda l: pid in l, important.rsplit('\n'))
+
+        if (nlines > 0):
+            lines = lines[-nlines:]
+        res = ''
+        for line in lines:
+            res += '%s<br>' % (line.replace('<breakline>', '<br>'))
+        return res
