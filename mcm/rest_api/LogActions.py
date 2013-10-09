fc384fd871b3e5aec54a5178eb4d154e7d765535
PdmV Service <pdmv.service@cern.ch>
2013-10-09 15:08:25 +0200

    mcm db

--- mcm/rest_api/LogActions.py
+++ mcm/rest_api/LogActions.py
@@ -3,7 +3,7 @@
 import cherrypy
 import re
 from json import loads, dumps
-from couchdb_layer.prep_database import database
+from couchdb_layer.mcm_database import database
 from RestAPIMethod import RESTResource
 
 
