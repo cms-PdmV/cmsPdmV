296e7cc2fa529cdd5bf66204d4f2e565ab4c1f13
PdmV Service <pdmv.service@cern.ch>
2013-10-09 10:36:58 +0200

    clean up

--- mcm/rest_api/RequestPrepId.py
+++ mcm/rest_api/RequestPrepId.py
@@ -34,34 +34,4 @@ class RequestPrepId(RESTResourceIndex):
 
     def generate_prepid(self, pwg, campaign):
         return dumps({"prepid": self.next_prepid(pwg, campaign)})
-        """
-        if not pwg:
-            self.logger.error('Physics working group provided is None.')
-            return dumps({"prepid":""})
-        if not campaign:
-            self.logger.error('Campaign prepid provided is None.') 
-            return dumps({"prepid":""})
 
-        # get the list of the prepids with the same pwg and campaign name 
-        res = map(lambda x: x['prepid'], self.db.queries(['pwg==%s'%(pwg),
-                                                          'member_of_campaign==%s'%(campaign)]))
-        if not res:
-            self.logger.log('Beginning new prepid family: %s' % (pwg+"-"+campaign+"-00001"), level='warning')
-            return dumps({"prepid":pwg+"-"+campaign+"-00001"})
-        
-        sn = -1
-        for pid in res:
-            thisSN = int(pid.rsplit('-')[2])
-            if  thisSN >= sn:
-                sn = thisSN+1
-        
-        if sn < 0:
-            # increase the serial number of the last request by one
-            sn = int(res[-1].rsplit('-')[2])+1
-            
-        new_prepid = pwg + '-' + campaign + '-' + str(sn).zfill(5)
-        self.logger.log('New prepid: %s' % (new_prepid), level='debug') 
-        
-        # return a json like: {'prepid': new_prepid}
-        return dumps({"prepid":new_prepid})
-        """
