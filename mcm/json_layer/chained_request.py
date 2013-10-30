742c1f69038d9efa863f5c5b66a9a86e692ac7d1
PdmV Service <pdmv.service@cern.ch>
2013-10-30 09:57:26 +0100

    move some check upstream to save some time in flow

--- mcm/json_layer/chained_request.py
+++ mcm/json_layer/chained_request.py
@@ -227,6 +227,37 @@ class chained_request(json_base):
             raise self.ChainedRequestCannotFlowException(self.get_attribute('_id'),
                                                          'the flow is getting into a MCReproc campaign but not size per event is specified')
 
+
+
+        ## check that it is allowed to flow
+        allowed_flow_approvals = ['flow', 'submit']
+        ###### cascade of checks
+        # if flow allows -> do it
+        ## else if chained_request allows -> do it
+        # check all approvals (if flow say yes -> allowing policy)
+        """
+        if not mcm_f.get_attribute('approval') in allowed_flow_approvals:
+            raise self.ChainedRequestCannotFlowException(self.get_attribute('_id'),
+                                                         'The flow (%s) is not in proper approval state (%s)'%( 
+                                                            mcm_f.get_attribute('prepid'),
+                                                            mcm_f.get_attribute('approval')))
+
+        if not self.get_attribute('approval') in allowed_flow_approvals:
+            raise self.ChainedRequestCannotFlowException(self.get_attribute('_id'),
+                                                         'The chained request (%s) is not in the proper approval state (%s)'% (
+                                                            self.get_attribute('_id'),
+                                                            self.get_attribute('approval')))
+        """
+        if not mcm_f.get_attribute('approval') in allowed_flow_approvals:
+            if not self.get_attribute('approval') in allowed_flow_approvals:
+                raise self.ChainedRequestCannotFlowException(self.get_attribute('_id'),
+                                                             'Neither the flow (%s) nor the chained request (%s) approvals allow for flowing' % (
+                                                               mcm_f.get_attribute('approval'),
+                                                               self.get_attribute('approval')))
+
+        if next_campaign.get_attribute('status') == 'stopped':
+            raise self.CampaignStoppedException(next_campaign_id)
+
         #what is going to be the required number of events for the next request
         #update the stats to its best
         current_request.get_stats()
@@ -262,34 +293,6 @@ class chained_request(json_base):
         #next_total_events = int(current_request.get_attribute('completed_events') * float(original_action_item['threshold'] / 100.))
 
 
-        ## check that it is allowed to flow
-        allowed_flow_approvals = ['flow', 'submit']
-        ###### cascade of checks
-        # if flow allows -> do it
-        ## else if chained_request allows -> do it
-        # check all approvals (if flow say yes -> allowing policy)
-        """
-        if not mcm_f.get_attribute('approval') in allowed_flow_approvals:
-            raise self.ChainedRequestCannotFlowException(self.get_attribute('_id'),
-                                                         'The flow (%s) is not in proper approval state (%s)'%( 
-                                                            mcm_f.get_attribute('prepid'),
-                                                            mcm_f.get_attribute('approval')))
-
-        if not self.get_attribute('approval') in allowed_flow_approvals:
-            raise self.ChainedRequestCannotFlowException(self.get_attribute('_id'),
-                                                         'The chained request (%s) is not in the proper approval state (%s)'% (
-                                                            self.get_attribute('_id'),
-                                                            self.get_attribute('approval')))
-        """
-        if not mcm_f.get_attribute('approval') in allowed_flow_approvals:
-            if not self.get_attribute('approval') in allowed_flow_approvals:
-                raise self.ChainedRequestCannotFlowException(self.get_attribute('_id'),
-                                                             'Neither the flow (%s) nor the chained request (%s) approvals allow for flowing' % (
-                                                               mcm_f.get_attribute('approval'),
-                                                               self.get_attribute('approval')))
-
-        if next_campaign.get_attribute('status') == 'stopped':
-            raise self.CampaignStoppedException(next_campaign_id)
 
         ## select what is to happened : [create, patch, use]
         next_id = None
