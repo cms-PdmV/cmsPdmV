86e1808e573bfc64bdea5e47dfba0a87469344c9
PdmV Service <pdmv.service@cern.ch>
2013-11-29 10:56:02 +0100

    clean start script

--- mcm/start_mcm.sh
+++ mcm/start_mcm.sh
@@ -1,44 +1,10 @@
 #!/usr/bin/env bash
 
-username=pdmvserv
-
-# check for root( root is needed to avoid SELINUX restrictions for sockets)
-#if ! id | grep -q "uid=0(root)" ; then
-#	echo "ERROR:  must be root in order to run this script"
-#	exit 1
-#fi
-
-# get kerberos credentials
-#klist 
-#if [ $? -eq 1 ]; then
-#    kinit $username
-#    aklog
-#fi
+# get a continuous afs token
 source kinit.sh &
 
+# kill existing
 kill -9 `ps -e -f  | grep main | grep python | awk '{print $2}'`
-
-# check CouchDB status
-couch_status=`/etc/init.d/couchdb status | grep running`
-
-if [ -z "$couch_status" ]; then
-	echo 'WARNING: CouchDB is not running. Trying to raise an instance...'
-	# fallback to raise an instance
-	couch_status=`sudo /etc/init.d/couchdb start | grep OK`
-	if [ -z "$couch_status" ]; then
-		echo 'ERROR: Could not raise CouchDB instance. Exiting...'
-		exit
-	else
-		echo 'INFO: CouchDB instance running!'
-	fi	
-fi
-
-# spawn Apache server
-#apache_status=`sudo /etc/init.d/httpd restart | grep OK`
-#if [ -z "$couch_status" ]; then
-#	echo 'ERROR: Could not raise Apache instance. Exiting...'
-#        exit
-#fi
 
 # start CherryPy
 python2.6 main.py
