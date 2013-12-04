6ae26add2c12e91a1eaccea56a2f8e96f7a312c5
PdmV Service <pdmv.service@cern.ch>
2013-12-04 10:26:19 +0100

    perf improvement for reserving a chain id serial number

--- mcm/rest_api/ChainedRequestPrepId.py
+++ mcm/rest_api/ChainedRequestPrepId.py
@@ -9,10 +9,12 @@ from json_layer.chained_request import chained_request
 
 # generates the next valid prepid 
 class ChainedRequestPrepId(RESTResourceIndex):
+    serial_number_cache = {}
     def __init__(self):
         self.ccamp_db_name = 'chained_campaigns'
         self.creq_db_name = 'chained_requests'
-    
+
+        
     def next_id(self, pwg, campaign):
         ccamp_db = database(self.ccamp_db_name)
         creq_db = database(self.creq_db_name)
@@ -27,17 +29,22 @@ class ChainedRequestPrepId(RESTResourceIndex):
                 self.logger.error('Campaign id {0} does not exist.'.format(campaign))
                 return None
 
-            # get the list of the prepids with the same pwg and campaign name
-            results = creq_db.queries(['member_of_campaign==%s'%(campaign),
-                                       'pwg==%s'%(pwg)])
-            results = map(lambda cr : int(cr['prepid'].split('-')[-1]), results)
             sn=1
-            if len(results):
-                # increase the biggest serial number by one
-                sn = max(results) + 1
-            
+            k = '%s_%s' %( pwg, campaign)
+            if k in self.serial_number_cache:
+                sn = self.serial_number_cache[k]
+            else:
+                # get the list of the prepids with the same pwg and campaign name
+                results = creq_db.queries(['member_of_campaign==%s'%(campaign),
+                                           'pwg==%s'%(pwg)])
+                results = map(lambda cr : int(cr['prepid'].split('-')[-1]), results)
+                if len(results):
+                    # increase the biggest serial number by one
+                    sn = max(results) + 1
+
             ## construct the new id
             new_prepid = pwg + '-' + campaign + '-' + str(sn).zfill(5)
+            self.serial_number_cache[k]=(sn+1)
             if sn==1:
                 self.logger.log('Beginning new prepid family: %s' % (new_prepid))
 
