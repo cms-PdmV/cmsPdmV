51d14d156d56a3ec71e34ebb0fbc1be5a0fcfd47
PdmV Service <pdmv.service@cern.ch>
2013-12-09 16:05:56 +0100

    speed up of request id reservation using serial number view of DB

--- mcm/rest_api/RequestPrepId.py
+++ mcm/rest_api/RequestPrepId.py
@@ -24,21 +24,19 @@ class RequestPrepId(RESTResourceIndex):
             return None
         with locker.lock("{0}-{1}".format(pwg, camp)):
             db = database(self.db_name)
-            res = map(lambda doc: int(doc['prepid'].split('-')[-1]), db.queries(['pwg==%s'%(pwg),
-                                                                                      'member_of_campaign==%s'%(camp)]))
-            lastSN=0
-            if len(res)!=0:
-                lastSN = max(res)
-            if lastSN==0:
-                self.logger.log('Beginning new prepid family: %s %s' %( pwg, camp))
-            lastSN+=1
-            pid='%s-%s-%05d'%( pwg, camp , lastSN)
-            db_camp = database('campaigns')
+            query_results = db.raw_query('serial_number', {'group':True, 'key':[camp, pwg]})
+            sn = 1
+            if query_results:
+                sn = query_results[0]['value']+1
+            pid='%s-%s-%05d'%( pwg, camp , sn)
+            if sn==1:
+                self.logger.log('Beginning new prepid family: %s-%s' %( pwg, camp))
+            db_camp = database('campaigns', cache=True)
             req_camp = campaign(db_camp.get(camp))
-            new_request = request(req_camp.add_request({'_id':pid, 'prepid':pid}))
+            new_request = request(req_camp.add_request({'_id':pid, 'prepid':pid, 'pwg':pwg, 'member_of_campaign':camp}))
             new_request.update_history({'action':'created'})
             db.save(new_request.json())
-            self.logger.log('New prepid : %s '%( pid))
+            self.logger.log('New prepid : %s '%pid)
             return pid
 
     def generate_prepid(self, pwg, campaign):
