3349f51bfac2818ad1ad7bf36a5819974c0e3ee6
PdmV Service <pdmv.service@cern.ch>
2014-02-05 13:37:29 +0100

    locking on communicating

--- mcm/tools/communicator.py
+++ mcm/tools/communicator.py
@@ -8,6 +8,7 @@ from tools.locator import locator
 from tools.logger import logfactory
 from collections import defaultdict
 from tools.settings import settings
+from tools.locker import locker
 class communicator:
     cache = {}
     logger = logfactory 
@@ -17,37 +18,38 @@ class communicator:
 
     def flush(self,Nmin):
         res=[]
-        for key in self.cache.keys():
-            (subject,addressee,sender)=key
-            if self.cache[key]['N'] <= Nmin: 
-                ## flush only above a certain amount of messages
-                continue
-            destination = addressee.split(COMMASPACE)
-            text = self.cache[key]['Text']
-            msg = MIMEMultipart()
-
-            msg['From'] = sender
-            msg['To'] = addressee
-            msg['Date'] = formatdate(localtime=True)
-            new_msg_ID = make_msgid()  
-            msg['Message-ID'] = new_msg_ID 
-            msg['Subject'] = subject
-
-            ## add a signature automatically
-            text += '\n\n'
-            text += 'McM Announcing service'
-            #self.logger.log('Sending a message from cache \n%s'% (text))
-            try:
-                msg.attach(MIMEText(text))
-                smtpObj = smtplib.SMTP()
-                smtpObj.connect()
-                smtpObj.sendmail(sender, destination, msg.as_string())
-                smtpObj.quit()
-                self.cache.pop(key)
-                res.append( subject )
-            except Exception as e:
-                print "Error: unable to send email", e.__class__
-        return res
+        with locker.lock('accumulating_notifcations'):
+            for key in self.cache.keys():
+                (subject,addressee,sender)=key
+                if self.cache[key]['N'] <= Nmin: 
+                    ## flush only above a certain amount of messages
+                    continue
+                destination = addressee.split(COMMASPACE)
+                text = self.cache[key]['Text']
+                msg = MIMEMultipart()
+                
+                msg['From'] = sender
+                msg['To'] = addressee
+                msg['Date'] = formatdate(localtime=True)
+                new_msg_ID = make_msgid()  
+                msg['Message-ID'] = new_msg_ID 
+                msg['Subject'] = subject
+                
+                ## add a signature automatically
+                text += '\n\n'
+                text += 'McM Announcing service'
+                #self.logger.log('Sending a message from cache \n%s'% (text))
+                try:
+                    msg.attach(MIMEText(text))
+                    smtpObj = smtplib.SMTP()
+                    smtpObj.connect()
+                    smtpObj.sendmail(sender, destination, msg.as_string())
+                    smtpObj.quit()
+                    self.cache.pop(key)
+                    res.append( subject )
+                except Exception as e:
+                    print "Error: unable to send email", e.__class__
+            return res
 
     def sendMail(self,
                  destination,
@@ -81,21 +83,22 @@ class communicator:
             msg['Subject'] = '[McM] ' + subject
 
         ### accumulate messages prior to sending emails
-        com__accumulate=settings.get_value('com_accumulate')
+        com__accumulate=settings().get_value('com_accumulate')
         if accumulate or com__accumulate:
-            # get a subject where the request name is taken out
-            subject_type=" ".join( filter(lambda w : w.count('-')!=2, msg['Subject'].split()) )
-            addressees = msg['To']
-            sendee = msg['From']
-            key = (subject_type, sendee, addressees)
-            if key in self.cache:
-                self.cache[key]['Text']+='\n'
-                self.cache[key]['Text']+=text
-                self.cache[key]['N']+=1
-            else:
-                self.cache[key] = {'Text' : text, 'N':1}
-            #self.logger.log('Got a message in cache %s'% (self.cache.keys()))
-            return new_msg_ID
+            with locker.lock('accumulating_notifcations'):
+                # get a subject where the request name is taken out
+                subject_type=" ".join( filter(lambda w : w.count('-')!=2, msg['Subject'].split()) )
+                addressees = msg['To']
+                sendee = msg['From']
+                key = (subject_type, sendee, addressees)
+                if key in self.cache:
+                    self.cache[key]['Text']+='\n'
+                    self.cache[key]['Text']+=text
+                    self.cache[key]['N']+=1
+                else:
+                    self.cache[key] = {'Text' : text, 'N':1}
+                #self.logger.log('Got a message in cache %s'% (self.cache.keys()))
+                return new_msg_ID
 
 
         ## add a signature automatically
