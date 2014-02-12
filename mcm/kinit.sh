cca4c4c13da5ce7a6c956e1762fdb00f37d256ba
PdmV Service <pdmv.service@cern.ch>
2014-02-12 11:01:23 +0100

    do not spam with commissioning mail

--- mcm/kinit.sh
+++ mcm/kinit.sh
@@ -2,7 +2,7 @@
 
 while [ 1 ]; do
     grep password  ~pdmvserv/private/credentials | cut -f2 -d: | kinit pdmvserv
-    klist  | mailx -s "kinit for pdmvserv" pdmvserv@cern.ch 
+    ##klist  | mailx -s "kinit for pdmvserv" pdmvserv@cern.ch 
     aklog
     sleep 40000
 done
