c3e593956231e8e740c910e8699b2b2a849f0b56
PdmV Service <pdmv.service@cern.ch>
2014-02-12 09:50:55 +0100

    color banner in confirmation pop-ups, and a ref fix

--- mcm/scripts/requests_ctrl.js
+++ mcm/scripts/requests_ctrl.js
@@ -539,10 +539,24 @@ function resultsCtrl($scope, $http, $location, $window){
   };
 
   /*Is Sure modal actions*/
+  $scope.stringToColour = function(str) {
+    //converts any string to hexadecimal color format
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    var colour = '#';
+    for (var i = 0; i < 3; i++) {
+        var value = (hash >> (i * 8)) & 0xFF;
+        colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
+  }  
   $scope.open_isSureModal = function(action, prepid){
     $scope.isSure_Modal = true;
     $scope.toggle_prepid = prepid;
     $scope.modal_action = action;
+    $scope.modal_color = $scope.stringToColour(action);
   };
 
   $scope.closeisSureModal = function(){
