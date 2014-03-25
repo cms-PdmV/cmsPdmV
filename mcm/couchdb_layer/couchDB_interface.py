28b2a6b2690d9c3d51e1b25317f62cf7d7e33f6c
PdmV Service <pdmv.service@cern.ch>
2014-03-25 14:12:51 +0100

    move to lucene for all search

--- mcm/couchdb_layer/couchDB_interface.py
+++ mcm/couchdb_layer/couchDB_interface.py
@@ -142,7 +142,6 @@ class Database():
         if "key" in options:
             options["key"] = '"'+str(options["key"])+'"'
         db_request = self.construct_request("%s/%s&%s" %(self.__dbname, viewname, self.to_json_query(options)))
-        print "### %s" % (db_request.get_full_url())
         data = self.opener.open(db_request)
         return threaded_loads(data.read())
 
