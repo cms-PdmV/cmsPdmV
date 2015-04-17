2df3b5c3eb95b0eaa2382ef1dd9cbd54d03b330b
Antanas Norkus <antanas.norkus@cern.ch>
2015-04-17 17:41:54 +0200

    Some minor adjustments to migration URLs

--- mcm/tools/locator.py
+++ mcm/tools/locator.py
@@ -9,25 +9,23 @@ class locator:
 
         if host in ['vocms085.cern.ch']: ## openstack -dev machine
             return True
-        elif host in ['vocms093', 'vocms087.cern.ch']:
-            return False ## cms-pdmv-mcmint has to be removed after migration
+        elif host in ['vocms093.cern.ch', 'vocms087.cern.ch']: ## prod and int machines
+            return False
         return True
 
     def isInt(self):
         host = os.environ['HOSTNAME']
 
-        if host in ['vocms087.cern.ch']:
+        if host in ['vocms087.cern.ch']: ## int machine
             return True
         else:
             return False
 
     def dbLocation(self):
         if self.isDev():
-            host = os.environ['HOSTNAME']
-            ## needed while migration to openstack is going
-            return 'http://vocms085.cern.ch:5984/'
+            return 'http://vocms085.cern.ch:5984/' ## dev instance
         else:
-            return 'http://vocms090:5984/'
+            return 'http://vocms090.cern.ch:5984/' ## prod instance
             #return 'http://188.184.23.164:5984/'
     def workLocation(self):
         if self.isDev():
