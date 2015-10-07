a03047bd5ab291dd9fd0f7ef3e793b18f46d2d4a
Antanas Norkus <antanas.norkus@cern.ch>
2015-10-07 13:44:34 +0200

    Some code pretifying in actions objects

--- mcm/json_layer/action.py
+++ mcm/json_layer/action.py
@@ -15,20 +15,20 @@ class action(json_base):
 
         def __str__(self):
             return 'Error: PrepId is not defined.'
-            
+
     class PrepIdDoesNotExistException(Exception):
-        def __init__(self,  pid):
+        def __init__(self, pid):
             self.pid = pid
             action.logger.error('prepid %s does not exist in the database.' % (self.pid))
         def __str__(self):
             return 'Error: PrepId ' + self.pid + ' does not exist in the database.'
-            
+
     class ChainedCampaignDoesNotExistException(Exception):
-        def __init__(self,  cid):
+        def __init__(self, cid):
             self.c = cid
             action.logger.error('chained_campaign %s does not exist in the database.' % (self.c))
         def __str__(self):
-            return 'Error: Chained Campaign '+ self.c + ' does not exist'
+            return 'Error: Chained Campaign ' + self.c + ' does not exist'
 
     _json_base__schema = {
         '_id': '',
@@ -47,7 +47,7 @@ class action(json_base):
         self.update(json_input)
         self.validate()
 
-    def update(self,  json_input):
+    def update(self, json_input):
         self._json_base__json = {}
         if not json_input:
             self._json_base__json = deepcopy(self._json_base__schema)
@@ -59,121 +59,121 @@ class action(json_base):
                     self._json_base__json[key] = deepcopy(self._json_base__schema[key])
             if '_rev' in json_input:
                 self._json_base__json['_rev'] = json_input['_rev']
-            
+
             # fix the member_of_campaign parameter
             if not self._json_base__json['member_of_campaign']:
                 if self._json_base__json['prepid']:
                     self._json_base__json['member_of_campaign'] = self._json_base__json['prepid'].rsplit('-')[1]
-    
+
     def find_chains(self):
         # validate request
         if not self.get_attribute('prepid'):
             raise self.PrepIdNotDefinedException()
-        
+
         # initialize db connections
         try:
             reqdb = database('requests')
             campaigndb = database('campaigns')
         except database.DatabaseAccessError as ex:
             return False
-        
+
         # validate prepid
         if not reqdb.document_exists(self.get_attribute('prepid')):
             raise self.PrepIdDoesNotExistException(self.get_attribute('prepid'))
-        
+
         # get campaign id
         req = request(json_input=reqdb.get(self.get_attribute('prepid')))
-        
+
         # check if campaign exists
         campid = req.get_attribute('member_of_campaign')
         if not campid:
-            self.logger.error('action %s has not a campaign defined' % (self.get_attribute('prepid')))     
-            raise ValueError('Error: Campaign was not set for',  self.get_attribute('prepid'))
+            self.logger.error('action %s has not a campaign defined' % (self.get_attribute('prepid')))
+            raise ValueError('Error: Campaign was not set for', self.get_attribute('prepid'))
         if not campaigndb.document_exists(campid):
             raise self.PrepIdDoesNotExistException(campid)
-        
+
         # get all chains
-        return self.__retrieve_chains(self.get_attribute('prepid'),  campid)
-    
+        return self.__retrieve_chains(self.get_attribute('prepid'), campid)
+
     # Returns true if it calculated the correct chains, False otherwise
-    def __retrieve_chains(self,  prepid,  campid):
+    def __retrieve_chains(self, prepid, campid):
         # initialize db connections
         try:
             chaindb = database('chained_campaigns')
             cdb = database('campaigns')
         except database.DatabaseAccessError as ex:
             return False
-        
+
         # get all chains
         # '>' & '>=' operators in queries for string keys return the same
         #candidate_chains = chaindb.query('prepid>=chain_'+campid)
         candidate_chains = chaindb.query('prepid>=chain_'+campid+'_',page_num=-1)
         candidate_chains.extend(chaindb.query('prepid==chain_'+campid,page_num=-1))
-        
-        
+
         # map only prepids
-        ccids = map(lambda x: x['_id'],  candidate_chains)
+        ccids = map(lambda x: x['_id'], candidate_chains)
         chains = self.get_attribute('chains')
         new_chains = {}
-        
+
         # cross examine (avoid deleted, keep new ones)
         for ccid in ccids:
             if ccid in chains:
                 new_chains[ccid] = chains[ccid]
             else:
-                new_chains[ccid] = {'flag' : False }
-        
+                new_chains[ccid] = {'flag' : False}
+
         # make persistent
-        self.set_attribute('chains',  new_chains)
-        
+        self.set_attribute('chains', new_chains)
+
         return True
 
     # select a chain ( set a priority block )
-    def select_chain(self,  cid):#,  value=1):
+    def select_chain(self, cid):#,  value=1):
         if cid not in self.get_attribute('chains'):
             raise self.ChainedCampaignDoesNotExistException(cid)
-        
+
         # set
         chains = self.get_attribute('chains')
         chains[cid]['flag'] = True #value
-        
+
         # make persistent
         self.set_attribute('chains',  chains)
-    
+
     # deselect a chain ( turn it to 0 )
-    def deselect_chain(self,  cid):
+    def deselect_chain(self, cid):
         # retrieve chains attribute
         chains = self.get_attribute('chains')
-        
+
         # check if chained_campaign is in the chain
         if cid not in chains:
             raise self.ChainedCampaignDoesNotExistException(cid)
-        
+
         chains[cid]['flag'] = False # deselect = set 0
-        
+
         # make persistent
-        self.set_attribute('chains',  chains)
-    
+        self.set_attribute('chains', chains)
+
     # remove a chain from the dictionary ( shows N/A)
-    def remove_chain(self,  cid):
+    def remove_chain(self, cid):
         if cid not in self.get_attribute('chains'):
             raise self.ChainedCampaignDoesNotExistException(cid)
-            
+
         chains = self.get_attribute('chains')
         new_chains = {}
-        
+
         # clear
         for c in chains:
             if c == cid:
                 continue
             new_chains[c] = chains[c]
-        
+
         # persistent
-        self.set_attribute('chains',  new_chains)
-        
+        self.set_attribute('chains', new_chains)
+
     def inspect_priority(self, forChains=None):
         ##JR: until put in the proper place
-        chains=self.get_attribute('chains')
+        action.logger.error('inspecting priority for action. forChains: %s' % (forChains))
+        chains = self.get_attribute('chains')
         crdb = database('chained_requests')
         okay = True
         for inCC in chains:
@@ -191,27 +191,27 @@ class action(json_base):
             #            else:
             #                self.logger.error('Could not set block %s to %s'%(chains[cc]['block_number'],cr.get_attribute('prepid')))
             ## new convention
-            if 'chains' in chains[inCC] and type(chains[inCC]['chains'])==dict:
+            if 'chains' in chains[inCC] and type(chains[inCC]['chains']) == dict:
                 for acr in chains[inCC]['chains']:
                     if forChains and not acr in forChains: continue
-                    bn=chains[inCC]['chains'][acr]['block_number']
-                    cr=chained_request(crdb.get(acr))
+                    action.logger.error('inCC: %s \nacr:%s' % (inCC, acr))
+                    bn = chains[inCC]['chains'][acr]['block_number']
+                    cr = chained_request(crdb.get(acr))
                     if bn:
-                        self.logger.log('Set priority block %s to %s'%( bn , acr))
+                        self.logger.log('Set priority block %s to %s' % (bn, acr))
                         if not cr.set_priority( bn ):
                             okay = False
                     else:
-                        self.logger.error('Could not set block %s to %s'%( bn , acr))
-                        
+                        self.logger.error('Could not set block %s to %s' % (bn, acr))
 
         rd = database('requests')
         if rd.document_exists(self.get_attribute('prepid')):
-            r= request(rd.get(self.get_attribute('prepid')))
+            r = request(rd.get(self.get_attribute('prepid')))
             self.set_attribute('dataset_name',r.get_attribute('dataset_name'))
 
         return okay
 
-    def get_chains( self, chained_campaigned_id):
+    def get_chains(self, chained_campaigned_id):
         chains = self.get_attribute('chains')[chained_campaigned_id]
         if 'chains' in chains:
             return chains['chains']
--- mcm/rest_api/ActionsActions.py
+++ mcm/rest_api/ActionsActions.py
@@ -28,11 +28,11 @@ class CreateAction(RESTResource):
         except request.IllegalAttributeName as ex:
             return dumps({"results":False})
 
-        self.logger.log('Building new action %s by hand...'%(mcm_a.get_attribute('_id')))
+        self.logger.log('Building new action %s by hand...' % (mcm_a.get_attribute('_id')))
 
         priority_set = mcm_a.inspect_priority()
 
-        saved = adb.update( mcm_a.json() )
+        saved = adb.update(mcm_a.json())
         if saved:
             if priority_set:
                 return {"results":True , "prepid": mcm_a.get_attribute('prepid')}
@@ -51,13 +51,13 @@ class UpdateAction(RESTResource):
         """
         update the action with the provided json content
         """
-        res = self.import_action( threaded_loads(cherrypy.request.body.read().strip()) )
+        res = self.import_action(threaded_loads(cherrypy.request.body.read().strip()))
         return dumps(res)
 
     def import_action(self, content):
         adb = database('actions')
         try:
-            mcm_a = action( content ) 
+            mcm_a = action(content)
         except request.IllegalAttributeName as ex:
             return {"results":False}
         self.logger.log('Updating action "%s" by hand...' % (mcm_a.get_attribute('_id')))
@@ -66,7 +66,7 @@ class UpdateAction(RESTResource):
         chains = mcm_a.get_attribute('chains')
         for (cc,content) in chains.items():
             for (o,value) in content.items():
-                if type(value)==dict:
+                if type(value) == dict:
                     for (cr,specs) in value.items():
                         for (s,spec) in specs.items():
                             if spec is None:
@@ -74,12 +74,11 @@ class UpdateAction(RESTResource):
                 else:
                     if value is None:
                         content.pop(o)
-        mcm_a.set_attribute('chains',chains)
 
-
+        mcm_a.set_attribute('chains', chains)
         priority_set = mcm_a.inspect_priority()
+        saved = adb.update(mcm_a.json())
 
-        saved = adb.update( mcm_a.json() )
         if saved:
             if priority_set:
                 return {"results":True , "prepid": mcm_a.get_attribute('prepid')}
@@ -99,7 +98,7 @@ class UpdateMultipleActions(UpdateAction):
         self.logger.log('Updating multiple actions')
         data = threaded_loads(cherrypy.request.body.read().strip())
 
-        results=[]
+        results = []
         for single_action in data:
             results.append(self.import_action(single_action))
 
@@ -140,7 +139,7 @@ class SelectChain(RESTResource):
 
         return dumps({'results':self.select_chain(args[0], args[1])})
 
-    def select_chain(self, aid,  chainid):
+    def select_chain(self, aid, chainid):
         db = database(self.db_name)
         self.logger.log('Selecting chain %s for action %s...' % (chainid, aid))
         # if action exists
@@ -173,7 +172,7 @@ class DeSelectChain(RESTResource):
 
     def deselect_chain(self, aid, chainid):
         db = database(self.db_name)
-        self.logger.log('Deselecting chain %s for action %s...' % (chainid, aid))        
+        self.logger.log('Deselecting chain %s for action %s...' % (chainid, aid))
         # if action exists
         if db.document_exists(aid):
             # initialize the object
@@ -198,48 +197,49 @@ class GenerateChainedRequests(RESTResource):
         if not args:
             self.logger.error('No arguments were given')
             return dumps({'results':'Error: No arguments were given'})
-        res=[]
-        for aid in args[0].split(','):
-            res.append( self.generate_request(aid))
 
-        if len(res)==1:
+        res = []
+        for aid in args[0].split(','):
+            res.append(self.generate_request(aid))
+
+        if len(res) == 1:
             return dumps(res[0])
         else:
             return dumps(res)
 
-    def generate_request(self, aid, reserve=False, with_notify=True,special=False):
+    def generate_request(self, aid, reserve=False, with_notify=True, special=False):
         adb = database('actions')
         ccdb = database('chained_campaigns')
         crdb = database('chained_requests')
         rdb = database('requests')
         if not adb.document_exists(aid):
-            return {'results':False, 'message':'%s does not exist'%(aid)}
+            return {'results' : False, 'message' : '%s does not exist'%(aid)}
 
         self.logger.log('Generating all selected chained_requests for action %s' % (aid))
         mcm_a = action(adb.get(aid))
         chains = mcm_a.get_attribute('chains')
-        hasChainsChanged=False 
+        hasChainsChanged = False
         new_chains = []
-        for cc in chains:  
+        for cc in chains:
             if 'flag' in chains[cc] and chains[cc]['flag']:
                 ## in the new convention, that means that something needs to be created
 
-                mcm_cc = chained_campaign( ccdb.get(cc) )
-                new_cr = mcm_cc.generate_request( aid )
+                mcm_cc = chained_campaign(ccdb.get(cc))
+                new_cr = mcm_cc.generate_request(aid)
                 if not 'chains' in chains[cc]:
                     chains[cc]['chains'] = {}
                 chains[cc]['chains'][new_cr['prepid']] = {}
-                to_transfer=['flag','threshold','staged','block_number']
+                to_transfer = ['flag', 'threshold', 'staged', 'block_number']
                 for item in to_transfer:
                     if item in chains[cc]:
                         chains[cc]['chains'][new_cr['prepid']] [item] = chains[cc][item]
                         chains[cc].pop(item)
-                hasChainsChanged=True
+                hasChainsChanged = True
                 ## get the root request
                 req = request(json_input=rdb.get(aid))
-                new_cr['last_status']= req.get_attribute('status')
-                if new_cr['last_status'] in ['submitted','done']:
-                    new_cr['status']='processing'
+                new_cr['last_status'] = req.get_attribute('status')
+                if new_cr['last_status'] in ['submitted', 'done']:
+                    new_cr['status'] = 'processing'
 
                 if special:
                     new_cr['approval'] = 'none'
@@ -249,33 +249,36 @@ class GenerateChainedRequests(RESTResource):
 
                 ## update the cc history
                 ccdb.update(mcm_cc.json())
-                new_chains.append( new_cr['prepid'] )
+                new_chains.append(new_cr['prepid'])
                 # then let the root request know that it is part of a chained request
-                inchains=req.get_attribute('member_of_chain')
+                inchains = req.get_attribute('member_of_chain')
                 inchains.append(new_cr['prepid'])
                 inchains.sort()
                 req.set_attribute('member_of_chain',list(set(inchains)))
-                req.update_history({'action': 'join chain', 'step': new_cr['prepid']})
+                req.update_history({'action' : 'join chain', 'step' : new_cr['prepid']})
                 if with_notify:
-                    req.notify("Request {0} joined chain".format(req.get_attribute('prepid')), "Request {0} has successfully joined chain {1}".format(req.get_attribute('prepid'), new_cr['prepid']), Nchild=0, accumulate=True)
-                mcm_a.update_history({'action':'add','step' : new_cr['prepid']})
+                    req.notify("Request {0} joined chain".format(req.get_attribute('prepid')),
+                            "Request {0} has successfully joined chain {1}".format(req.get_attribute('prepid'), new_cr['prepid']),
+                            Nchild=0, accumulate=True)
+
+                mcm_a.update_history({'action' : 'add', 'step' : new_cr['prepid']})
                 rdb.update(req.json())
 
         if hasChainsChanged:
             #the chains parameter might have changed
-            mcm_a.set_attribute('chains',chains)
+            mcm_a.set_attribute('chains', chains)
             adb.update(mcm_a.json())
 
         #and set priorities properly to all requests concerned
         priority_set = mcm_a.inspect_priority(forChains=new_chains)
 
         ## do the reservation of the whole chain ?
-        res=[]
+        res = []
         if reserve:
             for cr in new_chains:
-                mcm_cr = chained_request(crdb.get( cr ))
-                res.append(mcm_cr.reserve( limit = reserve))
-                crdb.update( mcm_cr.json())
+                mcm_cr = chained_request(crdb.get(cr))
+                res.append(mcm_cr.reserve(limit=reserve))
+                crdb.update(mcm_cr.json())
 
         if priority_set:
             return {"results":True , "prepid": mcm_a.get_attribute('prepid')}
@@ -297,7 +300,7 @@ class GenerateAllChainedRequests(GenerateChainedRequests):
         adb = database('actions')
         self.logger.log('Generating all possible (and selected) chained_requests...')
         allacs = adb.get_all(-1) # no pagination
-        res=[]
+        res = []
         for a in allacs:
             res.append(self.generate_request(a['key']))
 
@@ -312,64 +315,64 @@ class SetAction(GenerateChainedRequests):
         """
         Set the action and generate the chained request for /aid/alias or cc name/block#/stage of threshold
         """
-        if len(args)<3:
+        if len(args) < 3:
             return dumps("Not enough arguments")
 
         (aid,cc,block) = args[0:3]
 
 
-        extra=None
-        if len(args)==4:
-            extra=args[3]
-        staged=None
-        threshold=None
+        extra = None
+        if len(args) == 4:
+            extra = args[3]
+        staged = None
+        threshold = None
         if extra:
             if '.' in extra:
-                threshold=float(extra)
-                if threshold>1 or threshold<0:
-                    return dumps("A threshold is specified %s but not valid"%( threshold))
-                threshold=int(threshold * 100)
+                threshold = float(extra)
+                if threshold > 1 or threshold < 0:
+                    return dumps("A threshold is specified %s but not valid" % (threshold))
+                threshold = int(threshold * 100)
             else:
-                staged=int(extra)
+                staged = int(extra)
 
-        block=int(block)
+        block = int(block)
 
-        return dumps(self.set_action( aid, cc, block, staged, threshold))
+        return dumps(self.set_action(aid, cc, block, staged, threshold))
 
-    def set_action(self, aid, cc, block, staged=None, threshold=None,reserve=False,special=False):
+    def set_action(self, aid, cc, block, staged=None, threshold=None, reserve=False, special=False):
         adb = database('actions')
         ccdb = database('chained_campaigns')
 
-        mcm_a = action( adb.get(aid) )
+        mcm_a = action(adb.get(aid))
         ccs = ccdb.queries(['alias==%s'% cc])
         if not len(ccs):
             ccs = ccdb.queries(['prepid==%s'% cc])
         if not len(ccs) :
-            return {"results": False, "message": "%s not a chained campaigns"%( cc )}
+            return {"results": False, "message": "%s not a chained campaigns"%(cc)}
 
-        mcm_cc = chained_campaign( ccs[0] )
-        mcm_cc_name=mcm_cc.get_attribute('prepid')
+        mcm_cc = chained_campaign(ccs[0])
+        mcm_cc_name = mcm_cc.get_attribute('prepid')
 
-        chains = mcm_a.get_attribute('chains')        
+        chains = mcm_a.get_attribute('chains')
         if mcm_cc_name not in chains:
             #detect
             mcm_a.find_chains()
             chains = mcm_a.get_attribute('chains')
 
         if mcm_cc_name not in chains:
-            return {"results": False, "message": "Not able to find %s for %s"%( mcm_cc_name, aid)}
+            return {"results": False, "message": "Not able to find %s for %s"%(mcm_cc_name, aid)}
 
-        chains[mcm_cc_name].update( { "flag":True, "block_number" : block})
+        chains[mcm_cc_name].update({"flag" : True, "block_number" : block})
         if staged:
-            chains[mcm_cc_name]['staged']=staged
+            chains[mcm_cc_name]['staged'] = staged
         if threshold:
-            chains[mcm_cc_name]['threshold']=threshold
+            chains[mcm_cc_name]['threshold'] = threshold
 
         #set back the chains
-        mcm_a.set_attribute('chains',chains)
+        mcm_a.set_attribute('chains', chains)
 
         #save it since it is retrieved from scratch later
-        adb.save( mcm_a.json() )
+        adb.save(mcm_a.json())
 
         #and generate the chained requests
         return self.generate_request(aid, reserve=reserve, special=special)
@@ -386,38 +389,38 @@ class DetectChains(RESTResource):
         db = database('actions')
         if not args:
             return dumps(self.find_all_chains(db))
-        res=[]
-        aids=args[0].split(',')
-        if len(aids)==1:
-            res=self.find_chains(aids[0],db)
+        res = []
+        aids = args[0].split(',')
+        if len(aids) == 1:
+            res = self.find_chains(aids[0], db)
         else:
             for aid in aids:
-                res.append(self.find_chains(aid,db))
+                res.append(self.find_chains(aid, db))
 
         return dumps(res)
 
-    def find_chains(self,  aid, db):
+    def find_chains(self, aid, db):
         self.logger.log('Identifying all possible chains for action %s' % (aid))
-        
+
         try:
             ac = action(json_input=db.get(aid))
             ac.find_chains()
-            saved= db.update(ac.json())
-            return {'results':saved,'prepid': aid}
+            saved = db.update(ac.json())
+            return {'results' : saved, 'prepid' : aid}
         except Exception as ex:
-            return {'results':False, 'prepid': aid, 'message' :str(ex)}
+            return {'results' : False, 'prepid' : aid, 'message' : str(ex)}
 
     def find_all_chains(self, db):
         self.logger.log('Identifying all possible chains for all actions in the database...')
         aids = lambda x: x['prepid'] , db.queries([])
-        res=[]
+        res = []
         for aid in aids:
-            res.append(self.find_chains(aid, db))
+            res.append(self.find_chains(aid,db))
 
         return res
 
 from rest_api.RequestActions import RequestLister
-class ActionsFromFile(RequestLister,RESTResource): 
+class ActionsFromFile(RequestLister, RESTResource):
     def __init__(self):
         RequestLister.__init__(self)
         self.retrieve_db = database('actions')
@@ -429,5 +432,4 @@ class ActionsFromFile(RequestLister,RESTResource):
         """
         adb = database('actions')
         all_ids = self.get_list_of_ids(adb)
-        return dumps(self.get_objects( all_ids, adb))
-
+        return dumps(self.get_objects(all_ids, adb))
