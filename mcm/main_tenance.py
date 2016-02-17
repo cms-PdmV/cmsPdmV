be8ea88f16db7e02d3819fc62b15b4e573f9314f
Antanas Norkus <antanas.norkus@cern.ch>
2016-02-17 15:41:38 +0100

    Updated cherrypy conf to updated autoreload method. Fixed maintenance servers logging

--- mcm/main_tenance.py
+++ mcm/main_tenance.py
@@ -6,7 +6,8 @@ import json
 import time
 import shelve
 import subprocess
-from tools.logger import rest_formatter
+
+from tools.logger import UserFilter, MemoryFilter
 from rest_api.RestAPIMethod import RESTResourceIndex, RESTResource
 from rest_api.ControlActions import TurnOffServer
 
@@ -16,12 +17,10 @@ file_location = os.path.dirname(__file__)
 def maintenance_page(*args, **kwargs):
     return open(os.path.join(file_location,'HTML','maintenance.html'))
 
-#@cherrypy.expose
 class GetDummyUserRole(RESTResource):
     def GET(self, *args):
         return json.dumps({"username": "trained", "role_index": 1, "role": "monkey"})
 
-#@cherrypy.expose
 class GetDummyNews(RESTResource):
     def GET(self, *args):
         return json.dumps([{"announced":True, "subject":"McM is in maintenance", "text":"", "date" : time.strftime('%Y-%m-%d-%H-%M'),"author" : "support" }])
@@ -40,31 +39,41 @@ log = cherrypy.log
 log.error_file = None
 log.access_file = None
 
+logger = logging.getLogger()
+logger.setLevel(0)
+
 maxBytes = getattr(log, "rot_maxBytes", 10000000)
 backupCount = getattr(log, "rot_backupCount", 1000)
 fname = getattr(log, "rot_error_file", "logs/error.log")
 
-logger = logging.getLogger()
-logger.setLevel(0)
-
 # Make a new RotatingFileHandler for the error log.
 h = logging.handlers.RotatingFileHandler(fname, 'a', maxBytes, backupCount)
-#h.setLevel(logging.DEBUG)
-h.setFormatter(rest_formatter())
-log.error_log.addHandler(h)
+error_formatter = logging.Formatter(fmt='[%(asctime)s][%(user)s][%(levelname)s] %(message)s',
+        datefmt='%d/%b/%Y:%H:%M:%S')
 
-# set up custom ReST logger
-logger = logging.getLogger("rest_error")
-logger.addHandler(h)
+usr_filt = UserFilter()
+h.setFormatter(error_formatter)
+h.addFilter(usr_filt)
+error_logger = logging.getLogger("mcm_error")
+error_logger.addHandler(h)
+
 
 # Make a new RotatingFileHandler for the access log.
 fname = getattr(log, "rot_access_file", "logs/access.log")
 h = logging.handlers.RotatingFileHandler(fname, 'a', maxBytes, backupCount)
+rest_formatter = logging.Formatter(fmt='{%(mem)s} [%(asctime)s][%(user)s][%(levelname)s] %(message)s',
+        datefmt='%d/%b/%Y:%H:%M:%S')
+
+mem_filt = MemoryFilter()
 h.setLevel(logging.DEBUG)
-h.setFormatter(rest_formatter())
+h.setFormatter(rest_formatter)
+h.addFilter(usr_filt)
+h.addFilter(mem_filt)
 log.access_log.addHandler(h)
 
 def start():
+    logger = logging.getLogger("mcm_error")
+    logger.info("Maintenence service started")
     RESTResource.counter = shelve.open('.mcm_rest_counter')
 
 def stop():
