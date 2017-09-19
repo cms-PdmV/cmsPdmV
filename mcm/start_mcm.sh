925f0b56348e7ebdcac34cc912ae027c2cf14e23
Antanas Norkus <antanas.norkus@cern.ch>
2017-09-19 11:34:04 +0200

    Changed start script to work with local install of nodejs Added a profile file to source for local nodejs Added local nodejs folders to gitignore

--- mcm/start_mcm.sh
+++ mcm/start_mcm.sh
@@ -3,16 +3,28 @@
 # get a continuous afs token
 source kinit.sh &
 
-# kill existing
-#kill -9 `ps -e -f | grep main | grep python | awk '{print $2}'`
-#kill -9 `ps -e -f | grep mcm | grep -v $$ | awk '{print $2}'`
 
 # the current version
 revision=`git describe --abbrev=0`
 echo "running revision",$revision
+
 export MCM_REVISION=$revision
+if [ ! -r node-v8.5.0-linux-x64 ] ;
+then
+    echo "Nodejs was not installed locally. We are downloading it's binary"
+    ##TO-DO: do we download a specific version? can we get latest?
+    wget --quiet https://nodejs.org/dist/v8.5.0/node-v8.5.0-linux-x64.tar.xz
+    tar -xf node-v8.5.0-linux-x64.tar.xz
+    rm node-v8.5.0-linux-x64.tar.xz
+    source profile
+    npm install
+else
+    echo "Node is already installed. Sourcing profile"
+    source profile
+fi
+
 echo "running grunt"
-grunt
+node_modules/grunt/bin/grunt
 echo "started on: " `date`
 # start CherryPy
 python2.6 main.py
