550cb3d58fdaecd5baa77c4f672a5c3e263f586c
Roberto Carlos Rivera Martinez <rriveram@cern.ch>
2017-09-19 11:50:27 +0200

    refactoring

--- mcm/rest_api/NotificationActions.py
+++ mcm/rest_api/NotificationActions.py
@@ -10,25 +10,43 @@ from json_layer.notification import notification
 from tools.user_management import user_pack, roles, access_rights
 from tools.json import threaded_loads
 
-class CheckNotifications(RESTResource):
+class NotificationRESTResource(RESTResource):
     def __init__(self):
         self.access_limit = access_rights.user
+        user_p = user_pack()
+        self.username = user_p.get_username()
+        self.role = self.authenticator.get_user_role(self.username, email=user_p.get_email())
+
+    def fetch_action_objects(self, action_objects, object_type, page, limit):
+        db = database(object_type)
+        index = page * limit
+        action_objects_results = []
+        while(index < len(action_objects) and len(action_objects_results) < limit):
+            fetch = limit - len(action_objects_results)
+            fetch = fetch if fetch < 20 else 20
+            query = db.construct_lucene_query({'prepid': action_objects[index:fetch]}, boolean_operator="OR")
+            action_objects_results += db.full_text_search('search', query)
+            index += 20
+        return action_objects_results
+
+
+class CheckNotifications(NotificationRESTResource):
+    def __init__(self):
+        NotificationRESTResource.__init__(self)
 
     def GET(self):
         """
         Check for new notifications
         """
+        CheckNotifications.__init__(self)
         notifications_db = database('notifications')
-        user_p = user_pack()
-        username = user_p.get_username()
-        role = self.authenticator.get_user_role(username, email=user_p.get_email())
         query = notifications_db.construct_lucene_query({
-            'target_role' : role,
-            'targets' : username,
+            'target_role' : self.role,
+            'targets' : self.username,
         }, boolean_operator='OR')
         notifications = notifications_db.full_text_search('search', query, page=-1)
         users_db = database("users")
-        seen_notifications = set(users_db.get(username)['seen_notifications'])
+        seen_notifications = set(users_db.get(self.username)['seen_notifications'])
         frequency = {}
         unseen_counter = 0
         for notif in notifications:
@@ -42,65 +60,63 @@ class CheckNotifications(RESTResource):
         frequency['unseen'] = unseen_counter
         return dumps(frequency)
 
-class FetchNotifications(RESTResource):
+class FetchNotifications(NotificationRESTResource):
     def __init__(self):
-        self.access_limit = access_rights.user
+        NotificationRESTResource.__init__(self)
 
     def GET(self, **kwargs):
         """
         Fetch notifications of a specific group
         """
+        FetchNotifications.__init__(self)
         page = -1
         group = '*'
         if 'page' in kwargs:
             page = kwargs['page']
         if 'group' in kwargs:
             group = kwargs['group']
-        user_p = user_pack()
-        username = user_p.get_username()
-        role = self.authenticator.get_user_role(username, email=user_p.get_email())
         notifications_db = database('notifications')
         query = notifications_db.construct_lucene_complex_query([
-                ('target_role', {'value': role}),
-                ('targets', {'value': username, 'join_operator': 'OR'}),
+                ('target_role', {'value': self.role}),
+                ('targets', {'value': self.username, 'join_operator': 'OR'}),
                 ('group', {'value': group, 'join_operator': 'AND'})
         ])
         notifications = notifications_db.full_text_search('search', query, page=page, limit=10, sort="\_id")
         users_db = database("users")
-        seen_notifications = set(users_db.get(username)['seen_notifications'])
+        seen_notifications = set(users_db.get(self.username)['seen_notifications'])
         for notif in notifications:
             notif['seen'] = notif['_id'] in seen_notifications
         self.logger.info("Fetched notifications")
         return dumps({'notifications': notifications})
 
-class SaveSeen(RESTResource):
+class SaveSeen(NotificationRESTResource):
     def __init__(self):
-        self.access_limit = access_rights.user
+        NotificationRESTResource.__init__(self)
 
     def POST(self):
         """
         Save notification seen by the user
         """
+        SaveSeen.__init__(self)
         try:
              notification_id = threaded_loads(cherrypy.request.body.read().strip())['notification_id']
         except TypeError:
             return dumps({"results": False, "message": "Couldn't read body of request"})
         users_db = database('users')
-        user_p = user_pack()
-        username = user_p.get_username()
-        user_json = users_db.get(username)
+        user_json = users_db.get(self.username)
         user_json['seen_notifications'].append(notification_id)
         self.logger.info("Saved seen for notification %s" % notification_id)
         return dumps({"results": users_db.save(user_json)})
 
-class FetchGroupActionObjects(RESTResource):
+class FetchGroupActionObjects(NotificationRESTResource):
     def __init__(self):
-        self.access_limit = access_rights.user
+        NotificationRESTResource.__init__(self)
 
     def GET(self, **kwargs):
         """
         Get group action objects from notification
         """
+        FetchGroupActionObjects.__init__(self)
         if 'group' not in kwargs:
             return dumps({})
         group = kwargs['group']
@@ -110,13 +126,10 @@ class FetchGroupActionObjects(RESTResource):
             page = int(kwargs['page'])
         if 'limit' in kwargs:
             limit = int(kwargs['limit'])
-        user_p = user_pack()
-        username = user_p.get_username()
-        role = self.authenticator.get_user_role(username, email=user_p.get_email())
         notifications_db = database('notifications')
         query = notifications_db.construct_lucene_complex_query([
-                ('target_role', {'value': role}),
-                ('targets', {'value': username, 'join_operator': 'OR'}),
+                ('target_role', {'value': self.role}),
+                ('targets', {'value': self.username, 'join_operator': 'OR'}),
                 ('group', {'value': group, 'join_operator': 'AND'})
         ])
         notifications = notifications_db.full_text_search('search', query)
@@ -128,26 +141,19 @@ class FetchGroupActionObjects(RESTResource):
                 object_type = notif['object_type']
         if object_type == '' or len(action_objects) < 1:
             return dumps({})
-        index = page * limit
-        db = database(object_type)
-        action_objects_results = []
-        while(index < len(action_objects) and len(action_objects_results) < limit):
-            fetch = limit - len(action_objects_results)
-            fetch = fetch if fetch < 20 else 20
-            query = db.construct_lucene_query({'prepid': action_objects[index:fetch]}, boolean_operator="OR")
-            action_objects_results += db.full_text_search('search', query)
-            index += 20
+        action_objects_results = self.fetch_action_objects(action_objects, object_type, page, limit)
         self.logger.info("Fetched group action objects for group %s" % group)
         return dumps(action_objects_results)
 
-class SearchNotifications(RESTResource):
+class SearchNotifications(NotificationRESTResource):
     def __init__(self):
-        self.access_limit = access_rights.user
+        NotificationRESTResource.__init__(self)
 
     def GET(self, **kwargs):
         """
         Search text in title and message fields
         """
+        SearchNotifications.__init__(self)
         if 'search' not in kwargs:
             return dumps({})
         search = '*' + kwargs['search'] + '*'
@@ -157,13 +163,10 @@ class SearchNotifications(RESTResource):
             page = int(kwargs['page'])
         if 'limit' in kwargs:
             limit = int(kwargs['limit'])
-        user_p = user_pack()
-        username = user_p.get_username()
-        role = self.authenticator.get_user_role(username, email=user_p.get_email())
         notifications_db = database('notifications')
         query = notifications_db.construct_lucene_complex_query([
-                ('target_role', {'value': role}),
-                ('targets', {'value': username, 'join_operator': 'OR'}),
+                ('target_role', {'value': self.role}),
+                ('targets', {'value': self.username, 'join_operator': 'OR'}),
                 ('action_objects', {'value': search, 'join_operator': 'AND', 'open_parenthesis': True}),
                 ('title', {'value': search, 'join_operator': 'OR', 'close_parenthesis': True})
         ])
@@ -171,14 +174,15 @@ class SearchNotifications(RESTResource):
         self.logger.info("Searched text %s in notifications" % search)
         return dumps(notifications)
 
-class FetchActionObjects(RESTResource):
+class FetchActionObjects(NotificationRESTResource):
     def __init__(self):
-        self.access_limit = access_rights.user
+        NotificationRESTResource.__init__(self)
 
     def GET(self, **kwargs):
         """
         Get action objects from notification
         """
+        FetchActionObjects.__init__(self)
         if 'notification_id' not in kwargs:
             return dumps({})
         notification_id = kwargs['notification_id']
@@ -190,15 +194,6 @@ class FetchActionObjects(RESTResource):
             limit = int(kwargs['limit'])
         notifications_db = database('notifications')
         mcm_notification = notification(notifications_db.get(notification_id))
-        db = database(mcm_notification.get_attribute("object_type"))
-        index = page * limit
-        action_objects = mcm_notification.get_attribute("action_objects")
-        action_objects_results = []
-        while(index < len(action_objects) and len(action_objects_results) < limit):
-            fetch = limit - len(action_objects_results)
-            fetch = fetch if fetch < 20 else 20
-            query = db.construct_lucene_query({'prepid': action_objects[index:fetch]}, boolean_operator="OR")
-            action_objects_results += db.full_text_search('search', query)
-            index += 20
-        self.logger.info("Fetched action objects for notifications %s" % notification_id)
+        action_objects_results = self.fetch_action_objects(mcm_notification.get_attribute("action_objects"), mcm_notification.get_attribute("object_type"), page, limit)
+        self.logger.info("Fetched action objects for notification %s" % notification_id)
         return dumps(action_objects_results)
