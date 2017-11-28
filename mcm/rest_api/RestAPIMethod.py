d2cc118714e7d14ac3f7b2ee115c1b781316b3c3
Justinas Rumševičius <justinas.rumsevicius@cern.ch>
2017-11-28 14:52:51 +0100

    RestAPI index for flask using template

--- mcm/rest_api/RestAPIMethod.py
+++ mcm/rest_api/RestAPIMethod.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import logging
+import cgi
 
 from tools.user_management import access_rights, roles
 from tools.user_management import authenticator, user_pack
+from tools.locker import locker
 from flask_restful import Resource
-from flask import request, abort, make_response
+from flask import request, abort, make_response, current_app, render_template
 
 
 class RESTResource(Resource):
@@ -24,17 +26,18 @@ class RESTResource(Resource):
         self.content = content
 
     def before_request(self):
-        if self.access_limit is not None:
-            self.logger.info('Setting access limit to access_rights.%s (%s)' % (roles[self.access_limit], self.access_limit))
+        access_limit = self.__class__.access_limit
+        if access_limit is not None:
+            self.logger.info('Setting access limit to access_rights.%s (%s)' % (roles[access_limit], access_limit))
         elif request.method in self.limit_per_method:
-            self.access_limit = self.limit_per_method[request.method]
+            access_limit = self.limit_per_method[request.method]
         user_p = user_pack()
         if not user_p.get_username():
             # meaning we are going public, only allow GET.
             if 'public' not in request.path:
                 self.logger.error('From within %s, adfs-login not found: \n %s \n %s' % (self.__class__.__name__, str(request.headers), str(request.path) ))
         else:
-            if not authenticator.can_access(user_p.get_username(), self.access_limit):
+            if not authenticator.can_access(user_p.get_username(), access_limit):
                 if user_p.get_username() in self.access_user:
                     self.logger.error('User %s allowed to get through'% user_p.get_username())
                 else:
@@ -47,15 +50,15 @@ class RESTResource(Resource):
         return resp
 
     def count_call(self):
-        pass
         # counter for calls
-        # method = getattr(self, request.method, None)
-        # with locker.lock("rest-call-counter"):
-        #    key = method.im_class.__name__ + method.__name__
-        #    try:
-        #        RESTResource.call_counters[key] += 1
-        #    except KeyError:
-        #        RESTResource.call_counters[key] = 0
+        method = request.method
+        with locker.lock("rest-call-counter"):
+           key = self.__class__.__name__ + method
+           print(key)
+           try:
+               RESTResource.call_counters[key] += 1
+           except KeyError:
+               RESTResource.call_counters[key] = 0
 
 
 class RESTResourceIndex(RESTResource):
@@ -86,41 +89,63 @@ class RESTResourceIndex(RESTResource):
         return self.index()
 
     def index(self):
-        self.res = '<h1>REST API for McM<h2>'
         methods = ['GET', 'PUT', 'POST', 'DELETE']
-        self.res += "<table border='1'><thead><tr><th>Path</th><th>Function name</th>" + ''.join(
-            map(lambda s: '<th>%s method info</th><th>Access level</th><th>Calls counter</th>' % (s), methods)) + "</tr></thead><tbody>"
-        keys = self.__dict__.keys()
-        keys.sort()
+        data = []
 
-        for key in keys:
-            o = getattr(self, key)
-            self.res += "<tr>"
-            if hasattr(o, 'access_limit'):
-                self.res += "<td><a href=%s/><b>%s</b></a></td>" % (key, key)
-                self.res += ' <td>%s</td>' % (o.__class__.__name__)
-                limit = None
-                if o.access_limit is not None:
-                    limit = o.access_limit
-                for m in methods:
-                    if m in o.__class__.__dict__:
-                        if getattr(o, m).__doc__:
-                            self.res += '<td>%s</td>' % (getattr(o, m).__doc__)
-                        else:
-                            self.res += '<td><b>To be documented</b></td>'
-                        if limit is not None:
-                            self.res += '<td align=center>+%s</td>' % (roles[limit])
-                        else:
-                            self.res += '<td align=center>%s</td>' % (roles[self.limit_per_method[m]])
-                        try:
-                            c_key = o.__class__.__name__ + m
-                            c = RESTResource.call_counters[c_key]
-                        except KeyError:
-                            c = 0
-                        self.res += '<td align=center>{0}</td>'.format(c)
+        for rule in current_app.url_map.iter_rules():
+            func = current_app.view_functions.get(rule.endpoint)
+            if not hasattr(func, 'view_class'):
+                # print('Rule ' + rule.rule + ' endpoint ' + rule.endpoint + ' has no view_class - SKIPPING')
+                continue
+
+            function_dict = {}
+
+            rule_escaped = cgi.escape(rule.rule)
+            function_name = func.view_class.__name__
+            acc_limit = None
+
+            if hasattr(func.view_class, 'access_limit'):
+                acc_limit = getattr(func.view_class, 'access_limit')
+
+            # print('Rule ' + rule.rule)
+            # print('Endpoint ' + rule.endpoint)
+            # print('Rule methods: ' + str(rule.methods))
+            # print('Class name ' + func.view_class.__name__)
+            # print('Access limit? ' + str(acc_limit))
+            # print('Module ' + func.view_class.__module__)
+
+            function_dict['path'] = rule.rule
+            function_dict['function_name'] = function_name
+
+            methods_list = []
+            for m in methods:
+                if m in rule.methods:
+                    method_dict = {}
+                    method_dict['name'] = m
+                    
+                    method_doc = func.view_class.__dict__.get(m.lower()).__doc__
+                    if method_doc is None:
+                        method_doc = 'To be documented'
+                    method_dict['doc'] = method_doc
+
+                    if acc_limit is not None:
+                        method_dict['access_limit'] = roles[acc_limit]
                     else:
-                        self.res += '<td>&nbsp;</small> </td><td>&nbsp;</td><td>&nbsp;</small> </td>'
-                self.res += "</tr>"
+                        method_dict['access_limit'] = roles[func.view_class.limit_per_method[m]]
 
-        self.res += "</tbody></table>"
-        return self.res
+                    try:
+                        c_key = function_name + m
+                        c = RESTResource.call_counters[c_key]
+                    except KeyError:
+                        c = 0
+
+                    method_dict['call_counter'] = '%d' % (c)
+
+                    methods_list.append(method_dict)
+
+            function_dict['methods'] = methods_list
+
+            data.append(function_dict)
+
+        # print(data)
+        return self.output_text(render_template('restapi.html', data=data), 200, None)
