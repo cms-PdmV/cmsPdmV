43c902ec2d6d5a930943256fa1805a1c43d683b9
Justinas Rumševičius <justinas.rumsevicius@cern.ch>
2018-07-09 14:29:08 +0200

    Do not submit chain if any next request is none-new

--- mcm/json_layer/request.py
+++ mcm/json_layer/request.py
@@ -752,9 +752,39 @@ class request(json_base):
             for r in chain:
                 if r == self.get_attribute('prepid'):
                     continue  # no self checking
+
                 mcm_r = request(rdb.get(r))
                 # we can move on to submit if everything coming next in the chain is new
                 moveon_with_single_submit &= (mcm_r.get_attribute('status') in ['new'])
+
+        fdb = database('flows')
+        ccdb = database('chained_campaigns')
+        # Check if any of the requests in any chained requests are new to block automatic submission.
+        for c in self.get_attribute('member_of_chain'):
+            mcm_cr = crdb.get(c)
+            chain = mcm_cr['chain'][mcm_cr['step']:]
+            chained_campaign = ccdb.get(mcm_cr['member_of_campaign'])
+
+            # Flag means whether chained campaign is active. If it's not active, ignore it
+            if not chained_campaign.get('action_parameters', {}).get('flag', True):
+                self.logger.info('Chained campaign %s flag is off, skipping check' % (chained_campaign.get_attribute('prepid')))
+                continue
+
+            for r in chain:
+                if r == self.get_attribute('prepid'):
+                    # No self checking
+                    continue
+
+                mcm_r = request(rdb.get(r))
+                mcm_r_flow = flow(fdb.get(mcm_r.get_attribute('flown_with')))
+                if mcm_r_flow.get_attribute('approval') in ['submit', 'tasksubmit'] and mcm_r.get_attribute('status') in ['new']:
+                    raise self.WrongApprovalSequence(
+                        self.get_attribute('status'),
+                        'submit',
+                        'The request %s could not be submitted because following request %s is none-new and flow is "%s". Approve following requests first.' % (
+                            self.get_attribute('prepid'),
+                            mcm_r.get_attribute('prepid'),
+                            mcm_r_flow.get_attribute('approval')))
 
         for c in self.get_attribute('member_of_chain'):
             mcm_cr = crdb.get(c)
