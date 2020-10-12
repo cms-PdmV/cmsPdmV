3e13a82807bf80abe3281fd03959a383cb2d0ec7
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2020-10-12 20:25:22 +0200

    Remove unused files

--- mcm/kinit.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while [ 1 ]; do
-    grep password  /home/mcm/credentials | cut -f2 -d: | kinit pdmvserv
-    ##klist  | mailx -s "kinit for pdmvserv" pdmvserv@cern.ch 
-    aklog
-    sleep 60
-done
