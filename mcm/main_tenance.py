d392c10508497a45be13d02f5a8785c1a464112a
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2021-02-19 15:26:51 +0100

    Remove unused files

--- mcm/main_tenance.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, send_from_directory
-app = Flask(__name__)
-
-@app.route('/')
-def campaigns_html():
-    return send_from_directory('HTML', 'maintenance.html')
-
-app.run(host='0.0.0.0', port=443, threaded=True, ssl_context=('/etc/pki/tls/certs/localhost.crt','/etc/pki/tls/private/localhost.key'))
