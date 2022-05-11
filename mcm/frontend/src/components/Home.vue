5f381386ec60b2761549b76adf67cb3ea012570d
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2022-05-11 14:05:57 +0200

    Home page

--- mcm/frontend/src/components/Home.vue
+++ mcm/frontend/src/components/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <h1 class="page-title">Home</h1>
-    <v-card raised class="page-card"> </v-card>
-    <ul>
-      <li
-        v-for="routeName in ['campaigns', 'flows', 'chained_campaigns', 'requests', 'chained_requests']"
-        :key="routeName"
-      >
-        <router-link :to="routeName" custom>{{ routeName }}</router-link>
-      </li>
-    </ul>
+    <v-container>
+      <h1 class="page-title">Home</h1>
+      <v-card raised class="page-card pa-2">
+        <ul>
+          <li v-for="routeName in ['campaigns', 'flows', 'chained_campaigns', 'requests', 'chained_requests']" :key="routeName">
+            <router-link class="bold-hover" :to="routeName" custom>{{ capitalizeName(routeName) }}</router-link>
+          </li>
+        </ul>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
@@ -20,6 +20,12 @@ export default {
     return {};
   },
   created() {},
-  methods: {},
+  methods: {
+
+    capitalizeName: function(name) {
+      return name.charAt(0).toUpperCase() + name.slice(1).replaceAll('_', ' ');
+    }
+
+  },
 };
 </script>
