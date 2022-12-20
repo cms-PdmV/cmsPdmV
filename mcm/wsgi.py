4ef3a21e51e3b9fb0d6d7f40e22be7e88cb075a0
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2022-12-20 18:03:22 +0100

    Fix static css
    Import main package inside wsgi.py to ensure static dist is properly
    configure inside the Flask app instance

--- mcm/wsgi.py
+++ mcm/wsgi.py
@@ -1,9 +1,9 @@
 # Gunicorn configuration
-from main import set_app
+import main
 
 # Retrieve Flask app instance
 debug = True
-app, _, _ = set_app(debug=debug)
+app, _, _ = main.set_app(debug=debug)
 
 if __name__ == '__main__':
     app.run()
