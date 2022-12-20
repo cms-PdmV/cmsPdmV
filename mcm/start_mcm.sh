fa9265a609afd30af96c923bfcff20ce8e1bab69
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2022-12-20 15:28:44 +0100

    Set environment variables at start script

--- mcm/start_mcm.sh
+++ mcm/start_mcm.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Environment variables for McM
+COUCH_DB_CRED=...
+CERT_FILE=...
+KEY_FILE=...
+
+export COUCH_CRED=$COUCH_DB_CRED
+export USERCRT=$CERT_FILE
+export USERKEY=$KEY_FILE
+
 source kinit.sh &
 
 echo "Running grunt"
