87d2751f9d37e0a14961fa82111cb7b1ef2c444e
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2022-12-20 10:59:41 +0100

    Use variables to set credentials file

--- mcm/kinit.sh
+++ mcm/kinit.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+CRED_FILE=...
 while [ 1 ]; do
-    grep password /home/pdmvserv/private/credentials | cut -f2 -d: | kinit pdmvserv
+    grep password $CRED_FILE | cut -f2 -d: | kinit pdmvserv
     aklog
     sleep 120
 done
