9533e946ac854c78c425c1c4ae1b141ee781cdca
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2024-02-16 03:13:27 +0100

    Deploy the databases via GitHub Actions
    1. Configure and deploy CouchDB and CouchDB Lucene
    
    Currently, some dummy values are set for the credentials. In the future, repository secrets will be used to retrieve the runtime configuration.

---
+++ mcm/deploy/action.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This is only intended to be executed by the
+# GitHub Action runner.
+
+# Sets up the environment and deploys the components
+echo "Docker version $(docker version)"
+echo "Docker Compose version $(docker compose version)"
+
+echo 'Creating data folders'
+DATA_PATH="$HOME/container"
+mkdir -p "$DATA_PATH/couchdb" && \
+mkdir -p "$DATA_PATH/lucene/data" && mkdir -p "$DATA_PATH/lucene/config"
+chown -R "$(whoami):docker" $DATA_PATH && chmod -R 770 $DATA_PATH
+
+export COUCHDB_DATA="$DATA_PATH/couchdb"
+export LUCENE_DATA_PATH="$DATA_PATH/lucene/data"
+export LUCENE_CONF_PATH="$DATA_PATH/lucene/config"
+
+# Path to CouchDB Lucene config file
+TO_INI_PATH='deploy/couchdb-lucene.ini'
+cp $GITHUB_WORKSPACE/$TO_INI_PATH $LUCENE_CONF_PATH/
+
+# Deployment
+docker compose -f $GITHUB_WORKSPACE/deploy/mcm-databases.yml up -d
+echo "Waiting for $SECONDS_TO_WAIT seconds...."
+sleep $SECONDS_TO_WAIT
+docker ps -a
+
+echo "Services status..."
+echo "CouchDB:"
+curl -s "http://localhost:$COUCHDB_PORT/" | python3 -m json.tool
+echo "CouchDB Lucene"
+curl -s "http://localhost:$LUCENE_PORT/" | python3 -m json.tool
