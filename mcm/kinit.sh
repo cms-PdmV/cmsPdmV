ba2ac3845203be0e58e793e4a6ed59620c341f75
Geovanny González-Rodríguez <geovanny.gonzalez@cern.ch>
2025-01-31 12:25:25 +0100

    Update `kinit.sh` script
    Retrieve service account credentials via environment variables. Request
    and renew the tickets only if SSH sessions will use the auth strategy
    `gssapi-with-mic`.
    
    This is mainly used for deployments inside CERN VMs. Deployments via
    containers do not require this as tickets are managed using sidecar
    containers in OpenShift.

--- mcm/kinit.sh
+++ mcm/kinit.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+# Renew a Kerberos ticket for authenticating SSH sessions.
 
-while [ 1 ]; do
-    grep password /home/pdmvserv/private/credentials | cut -f2 -d: | kinit pdmvserv
-    aklog
-    sleep 120
-done
+if [ -n "${MCM_GSSAPI_WITH_MIC}" ]; then 
+    if [ -z "${MCM_SERVICE_ACCOUNT_USERNAME}" ]; then echo "MCM_SERVICE_ACCOUNT_USERNAME is not set. Provide the service account username"; exit 1; fi;
+    if [ -z "${MCM_SERVICE_ACCOUNT_PASSWORD}" ]; then echo "MCM_SERVICE_ACCOUNT_PASSWORD is not set. Provide the service account password"; exit 1; fi;
+    echo "Renewing a Kerberos ticket for user: ${MCM_SERVICE_ACCOUNT_USERNAME}"
+
+    while [ 1 ]; do
+        echo "${MCM_SERVICE_ACCOUNT_PASSWORD}" | kinit "${MCM_SERVICE_ACCOUNT_USERNAME}"
+        aklog
+        sleep 120
+    done
+fi
